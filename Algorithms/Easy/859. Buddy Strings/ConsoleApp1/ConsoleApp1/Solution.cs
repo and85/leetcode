b907682ab1eff1287e7ce3b3bb3330231305d267
and85 <andreytkach85@gmai.com>
2020-04-23 08:43:19 +0200

    859. Buddy Strings

---
+++ Algorithms/Easy/859. Buddy Strings/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public bool BuddyStrings(string a, string b)
+        {
+            if (a == null || b == null)
+                return false;
+
+            if (a.Length != b.Length)
+                return false;
+
+            if (a.Equals(b) && HasDuplicates(a))
+                return true;
+
+            int numberOfDiffs = 2;
+            char[] diffsA = new char[numberOfDiffs];
+            char[] diffsB = new char[numberOfDiffs];
+            int diffCounter = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i])
+                {
+                    if (diffCounter + 1 > numberOfDiffs)
+                        return false;
+
+                    diffsA[diffCounter] = a[i];
+                    diffsB[diffCounter] = b[i];
+                    diffCounter++;
+                }
+            }
+
+            if (diffCounter != numberOfDiffs)
+                return false;
+
+            if (diffsA[0] == diffsB[1] && diffsA[1] == diffsB[0])
+                return true;
+            else
+                return false;
+        }
+
+        private bool HasDuplicates(string str)
+        {
+            var uniqueLetters = new HashSet<char>(); 
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (uniqueLetters.Contains(str[i]))
+                    return true;
+                uniqueLetters.Add(str[i]);
+            }
+            return false;
+        }
+    }
+}
---
+++ Algorithms/Easy/859. Buddy Strings/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void BuddyStringsTest1()
+        {
+            // arrange
+            string a = "ab";
+            string b = "ba";
+
+            // act
+            bool actual = new Solution().BuddyStrings(a, b);
+
+            // assert
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod()]
+        public void BuddyStringsTest2()
+        {
+            // arrange
+            string a = "ab";
+            string b = "ab";
+
+            // act
+            bool actual = new Solution().BuddyStrings(a, b);
+
+            // assert
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod()]
+        public void BuddyStringsTest3()
+        {
+            // arrange
+            string a = "aa";
+            string b = "aa";
+
+            // act
+            bool actual = new Solution().BuddyStrings(a, b);
+
+            // assert
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod()]
+        public void BuddyStringsTest4()
+        {
+            // arrange
+            string a = "aaaaaaabc";
+            string b = "aaaaaaacb";
+
+            // act
+            bool actual = new Solution().BuddyStrings(a, b);
+
+            // assert
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod()]
+        public void BuddyStringsTest5()
+        {
+            // arrange
+            string a = "";
+            string b = "aa";
+
+            // act
+            bool actual = new Solution().BuddyStrings(a, b);
+
+            // assert
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod()]
+        public void BuddyStringsTest6()
+        {
+            // arrange
+            string a = "abab";
+            string b = "abab";
+
+            // act
+            bool actual = new Solution().BuddyStrings(a, b);
+
+            // assert
+            Assert.AreEqual(true, actual);
+        }
+    }
+}
