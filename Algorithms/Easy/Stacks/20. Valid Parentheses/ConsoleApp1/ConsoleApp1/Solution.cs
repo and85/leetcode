727be68bb0a3ad3d0a3419a3a623b3513a109b24
and85 <andreytkach85@gmai.com>
2020-04-23 23:21:52 +0200

    20. Valid Parentheses

---
+++ Algorithms/Easy/Stacks/20. Valid Parentheses/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    public class Solution
+    {
+        char[] _openBrackets = new char[3] { '(', '{', '[' };
+        char[] _closedBrackets = new char[3] { ')', '}', ']' };
+        public bool IsValid(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return true;
+
+            var openBracketsStack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char cur = s[i];
+                
+                if (Array.Exists(_openBrackets, el => el == cur))
+                    openBracketsStack.Push(cur);
+                else
+                {
+                    // closed brackets
+                    if (openBracketsStack.Count == 0)
+                        return false;
+
+                    char openBracket = openBracketsStack.Pop();
+
+                    int openIndex = Array.IndexOf(_openBrackets, openBracket);
+                    int closeIndex = Array.IndexOf(_closedBrackets, cur);
+
+                    if (openIndex != closeIndex)
+                        return false;
+                }
+            }
+
+            return (openBracketsStack.Count == 0);
+        }
+    }
+}
---
+++ Algorithms/Easy/Stacks/20. Valid Parentheses/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void IsValidTest1()
+        {
+            // arrange
+            string input = "()";
+
+            // act
+            bool actual = new Solution().IsValid(input);
+
+            // assert
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod()]
+        public void IsValidTest2()
+        {
+            // arrange
+            string input = "()[]{}";
+
+            // act
+            bool actual = new Solution().IsValid(input);
+
+            // assert
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod()]
+        public void IsValidTest3()
+        {
+            // arrange
+            string input = "(]";
+
+            // act
+            bool actual = new Solution().IsValid(input);
+
+            // assert
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod()]
+        public void IsValidTest4()
+        {
+            // arrange
+            string input = "([)]";
+
+            // act
+            bool actual = new Solution().IsValid(input);
+
+            // assert
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod()]
+        public void IsValidTest5()
+        {
+            // arrange
+            string input = "{[]}";
+
+            // act
+            bool actual = new Solution().IsValid(input);
+
+            // assert
+            Assert.AreEqual(true, actual);
+        }
+
+        public void IsValidTest6()
+        {
+            // arrange
+            string input = "[";
+
+            // act
+            bool actual = new Solution().IsValid(input);
+
+            // assert
+            Assert.AreEqual(false, actual);
+        }
+    }
+
+    
+}
