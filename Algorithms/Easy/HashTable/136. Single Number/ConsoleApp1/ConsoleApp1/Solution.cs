dfc998bf4eba31720c7f33d358d54fa588127a70
and85 <andreytkach85@gmai.com>
2020-04-25 23:24:29 +0200

    136. Single Number

---
+++ Algorithms/Easy/HashTable/136. Single Number/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            //for (int i = 1; i < nums.Length; i++)
+            //{
+            //    nums[0] ^= nums[i];
+            //}
+
+            //return nums[0];
+
+            int result = 0;
+            Array.ForEach(nums, x => nums[0] ^= x);
+            return result;
+        }
+    }
+}
---
+++ Algorithms/Easy/HashTable/136. Single Number/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod]
+        [DataRow(new int[]{2, 2, 1}, 1)]
+        [DataRow(new int[] { 4, 1, 2, 1, 2 }, 4)]
+        public void SingleNumberTest(int[] nums, int expected)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            int actual = solution.SingleNumber(nums);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
