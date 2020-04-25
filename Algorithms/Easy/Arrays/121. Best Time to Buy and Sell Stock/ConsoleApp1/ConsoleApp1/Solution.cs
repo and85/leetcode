ebe60b3d16a92f38cf7c88cd7b2e0d0f01d38cfd
and85 <andreytkach85@gmai.com>
2020-04-25 22:35:42 +0200

    121. Best Time to Buy and Sell Stock

---
+++ Algorithms/Easy/Arrays/121. Best Time to Buy and Sell Stock/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices.Length == 0)
+                return 0;
+
+            int maxProfit = 0;
+            int minBuy = prices[0];
+            int profit;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] < minBuy)
+                    minBuy = prices[i];
+                
+                if (prices[i] > minBuy)
+                {
+                    profit = prices[i] - minBuy;
+                    if (profit > maxProfit)
+                        maxProfit = profit;
+                }                   
+            }
+
+            return maxProfit;
+        }
+    }
+}
---
+++ Algorithms/Easy/Arrays/121. Best Time to Buy and Sell Stock/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod]
+        [DataRow(new[] { 7, 1, 5, 3, 6, 4 }, 5)]
+        [DataRow(new[] { 90, 150, 50, 60, 0 }, 60)]
+        [DataRow(new[] { 1, 150, 50, 60, 1000 }, 999)]
+        [DataRow(new[] { 1, 1, 1, 1, 1 }, 0)]
+        [DataRow(new[] { 7, 6, 4, 3, 1}, 0)]
+        [DataRow(new[] { 1, 2, 3, 4, 5 }, 4)]
+
+        public void MaxProfitTest(int[] input, int expected)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            int actual = solution.MaxProfit(input);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
