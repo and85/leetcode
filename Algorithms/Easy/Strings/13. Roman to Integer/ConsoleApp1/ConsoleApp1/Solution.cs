5a71a30ce3b1d648baaebbd2aec37436aad5e646
and85 <andreytkach85@gmai.com>
2020-04-26 21:58:39 +0200

    13. Roman to integer

---
+++ Algorithms/Easy/Strings/13. Roman to Integer/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int RomanToInt(string s)
+        {
+            var digits = GetDigits();
+            int result = 0;
+            int integerCandidate;
+            string latinTwoSymbols;
+            for (int i = 0; i < s.Length; i++)
+            {
+                integerCandidate = digits[s[i].ToString()];
+                if (i + 1 != s.Length)
+                {
+                    latinTwoSymbols = s[i].ToString() + s[i + 1];
+                    if (digits.ContainsKey(latinTwoSymbols))
+                    {
+                        integerCandidate = digits[latinTwoSymbols];
+                        i++;
+                    }
+                }
+                result += integerCandidate;
+            }
+
+            return result;
+        }
+
+        private Dictionary<string, int> GetDigits()
+        {
+            return new Dictionary<string, int>
+            {
+                { "I", 1 },
+                { "V", 5 },
+                { "X", 10 },
+                { "L", 50 },
+                { "C", 100 },
+                { "D", 500 },
+                { "M", 1000 },
+                { "IV", 4 },
+                { "IX", 9 },
+                { "XL", 40 },
+                { "XC", 90 },
+                { "CD", 400 },
+                { "CM", 900 }
+            };
+        }
+    }
+}
---
+++ Algorithms/Easy/Strings/13. Roman to Integer/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod()]
+        [DataRow("III", 3)]
+        [DataRow("IV", 4)]
+        [DataRow("IX", 9)]
+        [DataRow("LVIII", 58)]
+        [DataRow("MCMXCIV", 1994)]
+        public void RomanToIntTest(string input, int expected)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            int actual = solution.RomanToInt(input);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
