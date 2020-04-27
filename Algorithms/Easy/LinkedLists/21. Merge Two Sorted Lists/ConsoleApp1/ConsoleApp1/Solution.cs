de98b698221445e76dcad154adda1f48758758ec
and85 <andreytkach85@gmai.com>
2020-04-27 08:33:33 +0200

    21. Merge Two Sorted Lists

---
+++ Algorithms/Easy/LinkedLists/21. Merge Two Sorted Lists/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            ListNode startNode = new ListNode(0);
+            ListNode mergedList = startNode;
+
+            while (true)
+            {
+                if (l1 == null)
+                {
+                    mergedList.next = l2;
+                    break;
+                }
+                if (l2 == null)
+                {
+                    mergedList.next = l1;
+                    break;
+                }
+
+                if (l1.val < l2.val)
+                {
+                    mergedList.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    mergedList.next = l2;
+                    l2 = l2.next;
+                }
+
+                mergedList = mergedList.next;
+            }
+
+            return startNode.next;
+        }
+    }
+}
---
+++ Algorithms/Easy/LinkedLists/21. Merge Two Sorted Lists/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void MergeTwoListsTest()
+        {
+            // arrange
+            var l1 = new ListNode(1);
+            l1.next = new ListNode(2);
+            l1.next.next = new ListNode(4);
+            var l2 = new ListNode(1);
+            l2.next = new ListNode(3);
+            l2.next.next = new ListNode(4);
+
+            // act
+            var solution = new Solution();
+            var actual = solution.MergeTwoLists(l1, l2);
+
+            // assert
+       
+            
+            
+            
+           int a1 = actual.val;
+            int a2 = actual.next.val;
+            int a3 = actual.next.next.val;
+            int a4 = actual.next.next.next.val;
+            int a5 = actual.next.next.next.next.val;
+            int a6 = actual.next.next.next.next.next.val;
+
+            Assert.AreEqual(1, a1);
+            Assert.AreEqual(1, a2);
+            Assert.AreEqual(2, a3);
+            Assert.AreEqual(3, a4);
+            Assert.AreEqual(4, a5);
+            Assert.AreEqual(4, a6);
+        }
+    }
+}
