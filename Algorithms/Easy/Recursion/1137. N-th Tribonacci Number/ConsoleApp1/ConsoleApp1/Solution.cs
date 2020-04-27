5f9db2350ce4f26229286a7d159a4114ea9e0d05
and85 <andreytkach85@gmai.com>
2020-04-28 00:21:46 +0200

    1137. N-th Tribonacci Number

---
+++ Algorithms/Easy/Recursion/1137. N-th Tribonacci Number/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        //public int Tribonacci(int n)
+        //{
+        //    if (n == 0)
+        //        return 0;
+        //    if (n == 1)
+        //        return 1;
+        //    if (n == 2)
+        //        return 1;
+
+        //    return Tribonacci(n - 1) + Tribonacci(n - 2) + Tribonacci(n - 3);
+        //}
+        public int Tribonacci(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+            if (n == 2)
+                return 1;
+
+            int[] sums = new int[n + 1];
+            sums[0] = 0;
+            sums[1] = 1;
+            sums[2] = 1;
+            int res = 0;
+            for (int i = 3; i <= n; i++)
+            {
+                sums[i] = sums[i - 3] + sums[i - 2] + sums[i - 1];
+                res = sums[i];
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Algorithms/Easy/Recursion/1137. N-th Tribonacci Number/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod()]
+        [DataRow(4, 4)]
+        [DataRow(25, 1389537)]
+        public void TribonacciTest(int n, int expected)
+        {
+            var solution = new Solution();
+
+            // act
+            int actual = solution.Tribonacci(n);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
