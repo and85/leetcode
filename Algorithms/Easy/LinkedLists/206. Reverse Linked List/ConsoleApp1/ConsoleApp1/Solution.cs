b9ff3cba908810d15481c83b85128cd0d887175b
and85 <andreytkach85@gmai.com>
2020-04-28 22:53:59 +0200

    206. Reverse Linked List

---
+++ Algorithms/Easy/LinkedLists/206. Reverse Linked List/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /*
+    Reverse a singly linked list.
+
+    Example:
+
+    Input: 1->2->3->4->5->NULL
+    Output: 5->4->3->2->1->NULL
+    Follow up:
+
+    A linked list can be reversed either iteratively or recursively. Could you implement both?
+    */
+    public class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            var listNodeStack = new Stack<int>();
+            while (head != null)
+            {
+                listNodeStack.Push(head.val);
+                head = head.next;
+            }
+
+            ListNode res = null;
+            ListNode node = null;
+            ListNode prevNode = null;
+            while (listNodeStack.Count != 0)
+            {
+                int val = listNodeStack.Pop();
+                node = new ListNode(val);
+                
+                if (prevNode != null)
+                    prevNode.next = node;
+                prevNode = node;
+
+                if (res == null)
+                    res = node;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Algorithms/Easy/LinkedLists/206. Reverse Linked List/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void ReverseListTest()
+        {
+            // arrange
+            var l1 = new ListNode(1);
+            l1.next = new ListNode(2);
+            l1.next.next = new ListNode(3);
+            l1.next.next.next = new ListNode(4);
+            l1.next.next.next.next = new ListNode(5);
+
+            // act
+            var solution = new Solution();
+            var actual = solution.ReverseList(l1);
+
+            // assert
+        }
+    }
+}
