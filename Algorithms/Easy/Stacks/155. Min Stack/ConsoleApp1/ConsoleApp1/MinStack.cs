f480d732f9cf8ba9e28db5e74e0f9d95ecc0e169
and85 <andreytkach85@gmai.com>
2020-04-30 23:51:28 +0200

    155. Min Stack

---
+++ Algorithms/Easy/Stacks/155. Min Stack/ConsoleApp1/ConsoleApp1/MinStack.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MinStack
+    {
+        public class Node
+        {
+            public Node(int value, Node previous, Node next)
+            {
+                Value = value;
+                Previous = previous;
+                Next = next;
+            }
+            
+            public int Value;
+            public Node Previous;
+            public Node Next;
+            public int CurrentMinimum;
+        }
+
+        Node _currentListNode;
+
+        /** initialize your data structure here. */
+        public MinStack()
+        {
+
+        }
+
+        public void Push(int x)
+        {
+            if (_currentListNode == null)
+            {
+                _currentListNode = new Node(x, null, null);
+                _currentListNode.CurrentMinimum = x;
+            }
+            else
+            {
+                
+                var newNode = new Node(x, _currentListNode, null);
+                int newMinimum = (newNode.Previous.CurrentMinimum < x) ? newNode.Previous.CurrentMinimum : x;
+                newNode.CurrentMinimum = newMinimum;
+
+                _currentListNode.Next = newNode;
+                _currentListNode = newNode;
+            }
+        }
+
+        public void Pop()
+        {
+            if (_currentListNode != null)
+            {
+                Node previousNode = _currentListNode.Previous;
+                if (previousNode != null)
+                    previousNode.Next = null;
+                
+                _currentListNode = previousNode;
+            }
+        }
+
+        public int Top()
+        {
+            return _currentListNode.Value;
+        }
+
+        public int GetMin()
+        {
+            return _currentListNode.CurrentMinimum;
+        }
+    }
+}
---
+++ Algorithms/Easy/Stacks/155. Min Stack/ConsoleApp1/ConsoleApp1Tests/MinStackTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class MinStackTests
+    {
+        [TestMethod()]
+        public void MinStackTest1()
+        {
+            // arrange
+            var obj = new MinStack();
+            
+            // act
+            obj.Push(-2);
+            obj.Push(0);
+            obj.Push(-3);
+            int a1 = obj.GetMin();
+            obj.Pop();
+            int a2 = obj.Top();
+            int a3 = obj.GetMin();
+
+            // assert
+            Assert.AreEqual(a1, -3);
+            Assert.AreEqual(a2, 0);
+            Assert.AreEqual(a3, -2);
+        }
+
+        [TestMethod()]
+        public void MinStackTest2()
+        {
+            // arrange
+            var obj = new MinStack();
+                
+            // act
+            obj.Push(2147483646);
+            obj.Push(2147483646);
+            obj.Push(2147483647);
+            obj.Top();
+            obj.Top();
+            obj.GetMin();
+            obj.Pop();
+            obj.GetMin();
+            obj.Pop();
+            obj.Push(2147483647);
+            obj.Top();
+            obj.GetMin();
+            obj.Push(-2147483648);
+            obj.Top();
+            obj.GetMin();
+            obj.Pop();
+            obj.GetMin();
+            
+
+           
+            // assert
+        }
+    }
+}
