1b6c6fbc842b89ea01930951b5cf8d5dd1e09a28
and85 <andreytkach85@gmai.com>
2020-05-01 13:25:56 +0200

    58. Length of Last Word

---
+++ Algorithms/Easy/Strings/58. Length of Last Word/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        //public int LengthOfLastWord(string s)
+        //{
+        //    if (string.IsNullOrWhiteSpace(s))
+        //        return 0;
+
+        //    bool wordStarted = false;
+        //    int fisrtSymbol = 0;
+
+        //    for (int i = s.Length - 1; i >= 0; i--)
+        //    {
+        //        if (s[i] == ' ' && wordStarted)
+        //            return fisrtSymbol - i;
+
+        //        if (s[i] != ' ' && !wordStarted)
+        //        {
+        //            wordStarted = true;
+        //            fisrtSymbol = i;
+        //        }
+
+        //    }
+
+        //    if (wordStarted)
+        //        return fisrtSymbol + 1;
+
+        //    return 0;
+        //}
+        public int LengthOfLastWord(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+                return 0;
+
+            int counter = 0;
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == ' ' && counter != 0)
+                    return counter;
+
+                if (s[i] != ' ')
+                {
+                    counter++;
+                }
+
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ Algorithms/Easy/Strings/58. Length of Last Word/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod()]
+        [DataRow("Hello World", 5)]
+        [DataRow("a", 1)]
+        [DataRow("a ", 1)]
+        public void LengthOfLastWordTest(string input, int expected)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            int actual = solution.LengthOfLastWord(input);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
