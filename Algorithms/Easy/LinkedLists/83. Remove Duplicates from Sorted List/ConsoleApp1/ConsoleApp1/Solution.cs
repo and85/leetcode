08bdbf038a2ac656bb5f5fa94e4ce0b7beab3068
and85 <andreytkach85@gmai.com>
2020-05-07 23:54:24 +0200

    83. Remove duplicates from Sorted List

---
+++ Algorithms/Easy/LinkedLists/83. Remove Duplicates from Sorted List/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            ListNode headRef = new ListNode();
+            headRef.next = head;
+
+            while (head != null)
+            {
+                ListNode nextNode = head.next;
+                if (head.next != null && head.val == head.next.val)
+                {
+                    while (nextNode != null && nextNode.val == head.val)
+                    {
+                        nextNode = nextNode.next;
+                    }
+                    head.next = nextNode;
+                    
+                }
+                
+                head = head.next;
+            }
+
+            return headRef.next;
+        }
+    }
+}
---
+++ Algorithms/Easy/LinkedLists/83. Remove Duplicates from Sorted List/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void DeleteDuplicatesTest()
+        {
+            // arrange
+            ListNode node = new ListNode(1, new ListNode(1, new ListNode(1)));
+
+            // act
+            var solution = new Solution().DeleteDuplicates(node);
+
+            // assert
+        }
+    }
+}
