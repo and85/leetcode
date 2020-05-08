94033665ca5b8a472962952c2190b01440100acc
and85 <andreytkach85@gmai.com>
2020-05-08 23:12:45 +0200

    404. Sum of Left Leaves

---
+++ Algorithms/Easy/Trees/404. Sum of Left Leaves/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    // https://leetcode.com/problems/sum-of-left-leaves/
+    public class Solution
+    {
+        private int _sum = 0; 
+
+        public int SumOfLeftLeaves(TreeNode root)
+        {
+            TraverseTree(root, false);
+
+            return _sum;
+        }
+
+        private void TraverseTree(TreeNode root, bool isLeft)
+        {
+            if (root == null)
+                return;
+
+            if (root.left == null && root.right == null && isLeft)
+                _sum += root.val;
+
+            TraverseTree(root.left, true);
+            TraverseTree(root.right, false);
+        }
+    }
+}
---
+++ Algorithms/Easy/Trees/404. Sum of Left Leaves/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void SumOfLeftLeavesTest()
+        {
+            // arrange
+            /*
+               3
+              / \
+              9  20
+                /  \
+               15   7
+            */
+            var root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+            var solution = new Solution();
+
+            // act
+            int actual = solution.SumOfLeftLeaves(root);
+
+            // assert
+            Assert.AreEqual(24, actual);
+        }
+    }
+}
