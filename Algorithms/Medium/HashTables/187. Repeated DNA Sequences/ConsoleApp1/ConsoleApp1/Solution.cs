ce1e1e285c04bea898c587d26902a89959e0b806
Andrii.Tkach <andreytkach85@gmail.com>
2020-05-13 23:16:34 +0200

    187. Repeated DNA Sequences

---
+++ Algorithms/Medium/HashTables/187. Repeated DNA Sequences/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public IList<string> FindRepeatedDnaSequences(string s)
+        {
+            int n = 10;
+            var results = new List<string>();
+
+            if (s.Length <= n)
+                return results;
+            
+            var uniqueSubstrings = new HashSet<string>();
+            string substring = s.Substring(0, n);
+            uniqueSubstrings.Add(substring);
+
+            // O(N)
+            for (int i = 0; i < s.Length - n; i++)
+            {
+                int substringEndPosition = i + n;
+                // get a new substring in O(1) by removing 1 character and adding the
+                // next character from s, like in  Karp-Rabin algorithm
+                substring = substring.Substring(1, n - 1) + s[substringEndPosition];
+
+                if (!uniqueSubstrings.Contains(substring))
+                {
+                    uniqueSubstrings.Add(substring);
+                }
+                else
+                {
+                    if (!results.Contains(substring))
+                        results.Add(substring);
+                }
+            }
+
+            //uniqueSubstrings.To
+            return results;
+        }
+    }
+}
---
+++ Algorithms/Medium/HashTables/187. Repeated DNA Sequences/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void FindRepeatedDnaSequencesTest()
+        {
+            // arrange
+            string input = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+
+            // act
+            var solution = new Solution();
+            var actual = solution.FindRepeatedDnaSequences(input);
+
+            // assert
+            Assert.AreEqual("AAAAACCCCC", actual[0]);
+            Assert.AreEqual("CCCCCAAAAA", actual[1]);
+        }
+
+        [TestMethod()]
+        public void FindRepeatedDnaSequencesTest2()
+        {
+            // arrange
+            string input = "AAAAAAAAAAA";
+
+            // act
+            var solution = new Solution();
+            var actual = solution.FindRepeatedDnaSequences(input);
+
+            // assert
+            Assert.AreEqual("AAAAAAAAAA", actual[0]);
+        }
+    }
+}
