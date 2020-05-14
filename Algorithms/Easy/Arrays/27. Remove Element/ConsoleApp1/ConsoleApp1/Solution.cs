e0c3839ea3f6c597d9fb18b926384a75aa90bedf
Andrii.Tkach <andreytkach85@gmail.com>
2020-05-14 23:48:50 +0200

    27 and 349

---
+++ Algorithms/Easy/Arrays/27. Remove Element/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int duplicatesCounter = 0;
+            for (int i = 0; i < nums.Length - duplicatesCounter; i++)
+            {
+                if (nums[i] == val)
+                {
+                    for (int j = i + 1; j < nums.Length - duplicatesCounter; j++)
+                    {
+                        nums[j - 1] = nums[j];
+                    }
+                    duplicatesCounter++;
+                    i--;
+                }
+            }
+
+            return nums.Length - duplicatesCounter;
+        }
+    }
+}
---
+++ Algorithms/Easy/Arrays/349. Intersection of Two Arrays/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            var numHashSet = new HashSet<int>();
+            var resHashSet = new HashSet<int>();
+            foreach (int num in nums1)
+            {
+                if (!numHashSet.Contains(num))
+                    numHashSet.Add(num);
+            }
+
+            foreach (int num in nums2)
+            {
+                if (numHashSet.Contains(num))
+                    resHashSet.Add(num);
+            }
+
+            int[] res = new int[resHashSet.Count];
+            resHashSet.CopyTo(res);
+
+            return res;
+        }
+    }
+}
