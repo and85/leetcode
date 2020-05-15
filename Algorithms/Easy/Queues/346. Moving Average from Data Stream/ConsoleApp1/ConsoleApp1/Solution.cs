a23226f7f8dd4956334ad555cad027ac0482f056
Andrii.Tkach <andreytkach85@gmail.com>
2020-05-15 23:18:27 +0200

    346. Moving Average from Data Stream

---
+++ Algorithms/Easy/Queues/346. Moving Average from Data Stream/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class MovingAverage
+    {
+        private int _sum = 0;
+        private int _size;
+        private Queue<int> _nums;
+
+        public MovingAverage(int size)
+        {
+            _size = size;
+            _nums = new Queue<int>(size);
+
+        }
+
+        public double Next(int val)
+        {
+            _sum += val;
+            _nums.Enqueue(val);
+
+            if (_nums.Count > _size)
+            {
+                _sum -= _nums.Dequeue();
+            }
+
+            return (double)_sum / _nums.Count;
+        }
+    }
+}
