52d47a0f44f3840dda0d27aed92e6383624e8b7b
Andrii.Tkach <andreytkach85@gmail.com>
2020-05-16 00:05:31 +0200

    509

---
+++ Algorithms/Easy/Recursion/509. Fibonacci Number/ConsoleApp2/ConsoleApp2/Soluion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class Solution
+    {
+        public int Fib(int N)
+        {
+            if (N <= 1)
+                return N;
+
+            int prev = 0;
+            int sum = 1;
+           
+            int temp;
+
+            for (int i = 2; i <= N; i++)
+            {
+                temp = prev; 
+                prev = sum; 
+                sum = temp + sum; 
+             }
+
+            return sum;
+        }
+    }
+}
