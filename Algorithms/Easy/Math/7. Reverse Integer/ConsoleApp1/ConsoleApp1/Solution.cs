1a64a00b4fcd1074869c387045617d2c47a96674
Andrii.Tkach <andreytkach85@gmail.com>
2020-05-16 18:35:05 +0200

    7. Reverse Integer

---
+++ Algorithms/Easy/Math/7. Reverse Integer/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            bool isNegative = (x < 0);
+            if (isNegative)
+            {
+                x = -x;
+            }
+
+            int prevRevertedNumber = 0;
+            int revertedNumber = 0;
+
+            while (x != 0)
+            {
+                int curDigit = x % 10;
+
+                revertedNumber = (revertedNumber * 10) + curDigit;
+
+                if ((revertedNumber - curDigit) / 10 != prevRevertedNumber)
+                {
+                    return 0;
+                }
+
+                prevRevertedNumber = revertedNumber;
+                x = x / 10;
+            }
+
+            return (isNegative) ? -revertedNumber : revertedNumber;
+        }
+    }
+}
