af55d0a4034c3b632e582691d19f24c50ef5acb3
and85 <andreytkach85@gmai.com>
2020-05-22 08:37:29 +0200

    36. Valid Sudoku

---
+++ Algorithms/Medium/HashTables/36. Valid Sudoku/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        char EmptyPosition = '.';
+        const int SubBoxSize = 3;
+
+        public bool IsValidSudoku(char[][] board)
+        {
+            HashSet<char>[] hashSets = InitHashSetArray(board);
+
+            char currPosition;
+            int currSubBox;
+
+            int columnShift = board.Length;
+            int subBoxShift = board.Length * 2;
+            int jSubBoxShift;
+
+            for (int i = 0; i < board.Length; i++)
+            {
+                for (int j = 0; j < board[i].Length; j++)
+                {
+                    currPosition = board[i][j];
+                    jSubBoxShift = GetJSubBoxJhift(j);
+                    currSubBox = i / SubBoxSize + GetJSubBoxJhift(j);
+
+                    if (!IsHashSetValid(hashSets[i], currPosition) ||
+                        !IsHashSetValid(hashSets[j + columnShift], currPosition) ||
+                        !IsHashSetValid(hashSets[currSubBox + subBoxShift], currPosition))
+                    {
+                        return false;
+                    }
+                }
+            }
+            
+            return true;
+        }
+
+        private int GetJSubBoxJhift(int j)
+        {
+            return (j / SubBoxSize) * SubBoxSize;
+        }
+
+        private bool IsHashSetValid(HashSet<char> hashSet, char element)
+        {
+            if (!hashSet.Contains(element))
+            {
+                if (element != EmptyPosition)
+                    hashSet.Add(element);
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private HashSet<char>[] InitHashSetArray(char[][] board)
+        {
+            int boardSquare = board.Length * board.Length;
+            int subBoxSquare = SubBoxSize * SubBoxSize;
+            int subBoxNum = boardSquare / subBoxSquare;
+            // amount of columns + amount of rows + amount of sub-boxes
+            int uniqueHashSetNum = board.Length * 2 + subBoxNum;
+ 
+            HashSet<char>[] rowColumnHashSet = new HashSet<char>[uniqueHashSetNum];
+            for (int i = 0; i < uniqueHashSetNum; i++)
+                rowColumnHashSet[i] = new HashSet<char>();
+            return rowColumnHashSet;
+        }
+    }
+}
---
+++ Algorithms/Medium/HashTables/36. Valid Sudoku/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void IsValidSudokuTest()
+        {
+            // arrange
+            char[][] board = new char[][]
+            {
+                new char[] {'5','3','.','.','7','.','.','.','.'},
+                new char[] {'6','.','.','1','9','5','.','.','.'},
+                new char[] {'.','9','8','.','.','.','.','6','.'},
+                new char[] {'8','.','.','.','6','.','.','.','3'},
+                new char[] {'4','.','.','8','.','3','.','.','1'},
+                new char[] {'7','.','.','.','2','.','.','.','6'},
+                new char[] {'.','6','.','.','.','.','2','8','.'},
+                new char[] {'.','.','.','4','1','9','.','.','5'},
+                new char[] {'.','.','.','.','8','.','.','7','9'}
+
+            };
+
+            var solution = new Solution();
+            
+            // act
+            var actual = solution.IsValidSudoku(board);
+
+            //assert
+            Assert.IsTrue(actual);
+        }
+    }
+}
