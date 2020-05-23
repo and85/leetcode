24f1f6fbc45a9743f550265a809e766060f24175
and85 <andreytkach85@gmai.com>
2020-05-23 17:42:32 +0200

    292. Nim Game

---
+++ Algorithms/Medium/Design/ConsoleApp1/ConsoleApp1/LRUCache.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class LRUCache
+    {
+
+        public LRUCache(int capacity)
+        {
+
+        }
+
+        public int Get(int key)
+        {
+
+        }
+
+        public void Put(int key, int value)
+        {
+
+        }
+    }
+}
