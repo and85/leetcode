bbc88e3c9ecc6606c686ba0ec2f973f002986b9e
Andrii.Tkach <andreytkach85@gmail.com>
2020-05-24 22:51:06 +0200

    141. Linked List Cycle

---
+++ Algorithms/Easy/LinkedLists/141. Linked List Cycle/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+
+        public bool HasCycle(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (slow != null && fast != null)
+            {
+                slow = slow?.next;
+                fast = fast?.next?.next;
+
+                if (fast == slow)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
