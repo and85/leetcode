300b32d8ea60f6bff61ebd4775b7ef3e02c03dc8
Andrii.Tkach <andreytkach85@gmail.com>
2020-05-24 23:38:56 +0200

    231. Power of Two

---
+++ Algorithms/Easy/BitManipulations/231. Power of Two/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            // only one bit should be set for the power of 2
+            int setBitCounter = 0;
+            while (n != 0)
+            {
+                setBitCounter += n & 1;
+                if (setBitCounter > 1) return false;
+               
+                n >>= 1;
+            }
+            
+            return setBitCounter == 1;
+        }
+    }
+}
