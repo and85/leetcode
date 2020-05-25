4120db46be5613a9ec295daf89f1de8b27703f10
and85 <andreytkach85@gmai.com>
2020-05-25 23:38:52 +0200

    3. Longest Substring Without Repeating Characters

---
+++ Algorithms/Medium/Strings/3. Longest Substring Without Repeating Characters/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod()]
+        [DataRow("abcabcbb", 3)]
+        [DataRow("bbbbb", 1)]
+        [DataRow("pwwkew", 3)]
+        [DataRow(" ", 1)]
+        [DataRow("au", 2)]
+        [DataRow("jbpnbwwd", 4)]
+        public void LengthOfLongestSubstringTest(string input, int expected)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            int actual = solution.LengthOfLongestSubstring(input);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
