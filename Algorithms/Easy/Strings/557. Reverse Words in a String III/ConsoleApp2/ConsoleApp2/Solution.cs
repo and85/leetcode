b2307be9da710509cfaed162291f4cf905aa0e84
and85 <andreytkach85@gmai.com>
2020-05-26 22:49:01 +0200

    557. Reverse Words in a String III

---
+++ Algorithms/Easy/Strings/557. Reverse Words in a String III/ConsoleApp2/ConsoleApp2/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Solution
+    {
+        public string ReverseWords(string s)
+        {
+            var resultSb = new StringBuilder(s.Length);
+            var stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != ' ')
+                {
+                    stack.Push(s[i]);
+                }
+                else
+                {
+                    AppendResult(resultSb, stack);
+                    resultSb.Append(' ');
+                }
+            }
+
+            // last word of the sentence which may not contain spaces after it
+            AppendResult(resultSb, stack);
+
+            return resultSb.ToString();
+        }
+
+        private static void AppendResult(StringBuilder resultSb, Stack<char> stack)
+        {
+            while (stack.Count != 0)
+            {
+                resultSb.Append(stack.Pop());
+            }
+        }
+    }
+}
---
+++ Algorithms/Easy/Strings/557. Reverse Words in a String III/ConsoleApp2/ConsoleApp2Tests/SolutionTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod()]
+        [DataRow("Let's take LeetCode contest", "s'teL ekat edoCteeL tsetnoc")]
+        public void ReverseWordsTest(string input, string expected)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            string actual = solution.ReverseWords(input);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
