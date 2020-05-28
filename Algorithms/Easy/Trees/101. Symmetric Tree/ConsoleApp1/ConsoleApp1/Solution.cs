ce750636c07a8f052116bdc0851ec90c5b9e0644
and85 <andreytkach85@gmai.com>
2020-05-28 22:43:14 +0200

    101

--- Algorithms/Easy/Trees/101. Symmetric Tree/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Easy/Trees/101. Symmetric Tree/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -10,21 +10,7 @@ namespace ConsoleApp1
     {
         public bool IsSymmetric(TreeNode root)
         {
-            if (root == null) return true;
-
-            if (root.left == null && root.right == null )
-                return true;
-
-            if (root.left == null || root.right == null) 
-                return false;
-
-            if (root.left.left == null && root.right.right == null && root.left.right == null && root.right.left == null)
-                return root.left.val == root.right.val;
-
-            if (root.left.left?.val != root.right.right?.val || root.left.right?.val != root.right.left?.val)
-                return false;
-
-            return IsSymmetric(root.left) && IsSymmetric(root.right);
+            
         }
     }
 }
