740296393cd69728ef4651ac2071e54ba4da298a
and85 <andreytkach85@gmai.com>
2020-05-28 23:28:25 +0200

    344. Reverse String

---
+++ Algorithms/Easy/TwoPointers/344. Reverse String/ConsoleApp1/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public void ReverseString(char[] s)
+        {
+            int left = 0, right = s.Length - 1;
+            while (left < right)
+            {
+                (s[left], s[right]) = (s[right--], s[left++]);
+            }
+        }
+    }
+}
---
+++ Algorithms/Easy/TwoPointers/344. Reverse String/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod()]
+        [DataRow("hello", "olleh")]
+        [DataRow("Hannah", "hannaH")]
+        public void ReverseStringTest(string input, string expected)
+        {
+            // arrange
+            var solution = new Solution();
+            char[] charArray = input.ToCharArray();
+
+            // act
+             solution.ReverseString(charArray);
+
+            // assert
+            Assert.AreEqual(expected, new string(charArray));
+        }
+    }
+}
