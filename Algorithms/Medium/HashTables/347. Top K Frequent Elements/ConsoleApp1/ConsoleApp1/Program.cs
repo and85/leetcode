47756765495bef840c1dcd260393576e48ccea6f
and85 <andreytkach85@gmai.com>
2020-05-31 13:54:13 +0200

    347. Top K Frequent Elements

---
+++ Algorithms/Medium/HashTables/347. Top K Frequent Elements/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 4, 4, 4, 4, 1, 1, 1, 2, 2, 3 };
+            var solution = new Solution();
+            solution.TopKFrequent(nums, 2);
+
+            //int[] nums = new int[] { -1, -1 };
+            //var solution = new Solution();
+            //solution.TopKFrequent(nums, 1);
+        }
+    }
+}
---
+++ Algorithms/Medium/HashTables/347. Top K Frequent Elements/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            var entriesFrequency = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                if (!entriesFrequency.ContainsKey(num))
+                    entriesFrequency.Add(num, 1);
+                else
+                    entriesFrequency[num]++;
+            }
+
+            return entriesFrequency.OrderByDescending(o => o.Value)
+                .Take(k)
+                .Select(f => f.Key)
+                .ToArray();
+        }
+    }
+}
