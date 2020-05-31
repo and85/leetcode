e412347f936b85eeac9b3c6a5788d0045eb0ca56
and85 <andreytkach85@gmai.com>
2020-05-31 15:57:47 +0200

    19. Remove Nth Node from end of List

---
+++ Algorithms/Medium/LinkedLists/19. Remove Nth Node From End of List/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            var head = new ListNode(1);
+            head.next = new ListNode(2); 
+            solution.RemoveNthFromEnd(head, 1);
+        }
+    }
+}
---
+++ Algorithms/Medium/LinkedLists/19. Remove Nth Node From End of List/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            if (head == null)
+                return null;
+
+            int lenght = GetListLenght(head);
+            int jumpIndex = lenght - n;
+
+            if (jumpIndex != 0)
+            {
+                int nodeIndex = 0;
+                ListNode curr = head;
+                while (curr.next != null)
+                {
+                    if (++nodeIndex != jumpIndex)
+                        curr = curr.next;
+                    else
+                    {
+                        curr.next = curr.next.next;
+                        curr = curr.next;
+                    }
+                }
+            }
+            else
+            {
+                head = head.next;
+            }
+
+            return head;
+        }
+
+        private int GetListLenght(ListNode head)
+        {
+            int lenght = 1;
+            ListNode curr = head;
+            while (curr.next != null)
+            {
+                lenght++;
+                curr = curr.next;
+            }
+
+            return lenght;
+        }
+    }
+}
