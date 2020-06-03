11bcd00a44071b06c6b4dca59fe3a4cc66bfc379
and85 <andreytkach85@gmai.com>
2020-06-03 23:32:09 +0200

    252. Meeting Rooms

---
+++ Algorithms/Easy/Sort/252. Meeting Rooms/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] intervals = new int[3][];
+            intervals[0] = new int[2] { 0, 30 };
+            intervals[1] = new int[2] { 5, 10 };
+            intervals[2] = new int[2] { 15, 20 };
+
+            var solution = new Solution();
+            solution.CanAttendMeetings(intervals);
+
+        }
+    }
+}
---
+++ Algorithms/Easy/Sort/252. Meeting Rooms/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public bool CanAttendMeetings(int[][] intervals)
+        {
+            SortByIntervalStart(intervals);
+
+            for (int i = 1; i < intervals.Length; i++)
+            {
+                if (IsStartOverlapWithPreviousEnd(intervals, i))
+                    return false;
+            }
+
+            return true;
+        }
+
+        private bool IsStartOverlapWithPreviousEnd(int[][] intervals, int i)
+        {
+            return intervals[i][0] <= intervals[i - 1][1];
+        }
+
+        private void SortByIntervalStart(int[][] intervals)
+        {
+            Array.Sort(intervals, new Comparison<int[]>(
+                (x, y) => { return x[0] < y[0] ? -1 : (x[0] > y[0] ? 1 : 0); }
+                ));
+        }
+    }
+}
