470e7c535bed4c3ff513349292808dda276bdf46
and85 <andreytkach85@gmai.com>
2020-06-07 12:29:37 +0200

    237. Delete Node in a Linked List

---
+++ Algorithms/Easy/LinkedLists/237. Delete Node in a Linked List/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
