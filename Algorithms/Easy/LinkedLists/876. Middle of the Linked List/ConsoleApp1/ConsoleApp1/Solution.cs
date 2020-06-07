dbadcab57e6e726956a065ee8eb89ff5baf22c7d
and85 <andreytkach85@gmai.com>
2020-06-07 22:48:11 +0200

    876. Middle of the LinkedLists

--- Algorithms/Easy/LinkedLists/876. Middle of the Linked List/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Easy/LinkedLists/876. Middle of the Linked List/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -10,7 +10,16 @@ namespace ConsoleApp1
     {
         public ListNode MiddleNode(ListNode head)
         {
+            ListNode slow = head;
+            ListNode fast = head;
 
+            while (fast?.next != null)
+            {
+                slow = slow.next;
+                fast = fast?.next?.next;
+            }
+
+            return slow;
         }
     }
 }
