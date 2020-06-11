738c32b63c15c5904477ba1a73fd17b65291d6e3
Andrii.Tkach <andreytkach85@gmail.com>
2020-06-11 12:34:56 +0200

    11. Container with Most Water

--- Algorithms/Medium/Array/11. Container With Most Water/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Medium/Array/11. Container With Most Water/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -8,7 +8,49 @@ namespace ConsoleApp1
     {
         public int MaxArea(int[] height)
         {
-            return 0;
+            //return BruteForce(height);
+            return TwoPointers(height);
+        }
+
+        private int TwoPointers(int[] height)
+        {
+            int currArea = 0, maxArea = 0;
+            int left = 0;
+            int right = height.Length - 1;
+
+            while (left < right)
+            {
+                currArea = Square(height[left], height[right], right - left);
+                maxArea = Math.Max(maxArea, currArea);
+
+                if (height[left] <= height[right])
+                    left++;
+                else
+                    right--;
+            }
+
+            return maxArea;
+        }
+
+        private int BruteForce(int[] height)
+        {
+            int maxArea = 0;
+            int currArea = 0;
+            for (int i = 0; i < height.Length; i++)
+                for (int j = i + 1; j < height.Length; j++)
+                {
+                    currArea = Square(height[i], height[j], j - i);
+                    if (currArea > maxArea)
+                        maxArea = currArea;
+
+                }
+
+            return maxArea;
+        }
+
+        public int Square(int line1, int line2, int distance)
+        {
+            return Math.Min(line1, line2) * distance;
         }
     }
 }
---
+++ Algorithms/Medium/Array/11. Container With Most Water/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod()]
+        public void MaxAreaTest1()
+        {
+            // arrange 
+            int[] input = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7};
+            var solution = new Solution();
+
+            // act
+            int actual = solution.MaxArea(input);
+
+            // assert
+            Assert.AreEqual(49, actual);
+        }
+
+        [DataTestMethod()]
+        public void MaxAreaTest2()
+        {
+            // arrange 
+            int[] input = new int[] { 1, 1 };
+            var solution = new Solution();
+
+            // act
+            int actual = solution.MaxArea(input);
+
+            // assert
+            Assert.AreEqual(1, actual);
+        }
+    }
+}
