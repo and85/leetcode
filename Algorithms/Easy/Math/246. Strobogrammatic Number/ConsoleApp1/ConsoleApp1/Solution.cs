b7633a05fe9779244e69aa3ce9d7ce9900508268
Andrii.Tkach <andreytkach85@gmail.com>
2020-06-14 00:06:24 +0200

    246. Strobogrammatic Number

---
+++ Algorithms/Easy/Math/246. Strobogrammatic Number/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public bool IsStrobogrammatic(string num)
+        {
+            int i = 0, j = num.Length - 1;
+
+            while (i <= j)
+            {
+                if ((num[i] == '6' && num[j] == '9') ||
+                    (num[i] == '9' && num[j] == '6') ||
+                    (num[i] == '8' && num[j] == '8') ||
+                    (num[i] == '1' && num[j] == '1') ||
+                    (num[i] == '0' && num[j] == '0')
+                    )
+                {
+                    i++;
+                    j--;
+                    continue;
+                }
+                else
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
