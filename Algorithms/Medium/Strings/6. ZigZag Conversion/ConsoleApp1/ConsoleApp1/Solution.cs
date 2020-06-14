6d384a18600ff645c6a7ff38694b07e867beb684
Andrii.Tkach <andreytkach85@gmail.com>
2020-06-14 17:28:27 +0200

    6. ZigZag Conversion

--- Algorithms/Medium/Strings/6. ZigZag Conversion/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Medium/Strings/6. ZigZag Conversion/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -4,11 +4,99 @@ using System.Text;
 
 namespace ConsoleApp1
 {
+    // yeah, I unerstand, there should be one pass formula based solution which uses O(1) memory... This one is quite nasty and covers edge cases
     public class Solution
     {
+        enum MoveDirection
+        {
+            Up,
+            Down
+        }
+
         public string Convert(string s, int numRows)
         {
-            return null;
+            var grid = new List<char[]>();
+            char[] currArray = null;
+
+            MoveDirection direction = MoveDirection.Down;
+            int xPos = 0, yPos = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (direction == MoveDirection.Down)
+                {
+                    if (numRows != 2 || yPos == 0)
+                    {
+                        if (yPos <= 0)
+                        {
+                            currArray = new char[numRows];
+                            grid.Add(currArray);
+                        }
+
+                        yPos = (yPos >= 0) ? yPos : 0;
+                        currArray[yPos] = s[i];
+                        yPos++;
+                    }
+                    else
+                    {
+                        if (yPos == -1)
+                            yPos = 1;
+
+                        currArray[yPos] = s[i];
+                        yPos++;
+                    }
+                }
+
+                if (direction == MoveDirection.Up)
+                {
+                    currArray = new char[numRows];
+                    grid.Add(currArray);
+
+                    if (yPos == numRows)
+                    {
+                        // -1 to cover next row shift, - 1 to cover counting from zero
+                        yPos = numRows - 2;
+                        yPos = (yPos >= 0) ? yPos : 0; 
+                    }
+
+                    currArray[yPos] = s[i];
+                    yPos--;
+                }
+
+                direction = SetMoveDirection(numRows, direction, yPos);
+            }
+
+            return BuildResultString(grid, numRows);
+        }
+
+        private MoveDirection SetMoveDirection(int numRows, MoveDirection direction, int yPos)
+        {
+            if (yPos <= 0)
+            {
+                direction = MoveDirection.Down;
+            }
+
+            if (yPos == numRows)
+            {
+                direction = MoveDirection.Up;
+            }
+
+            return direction;
+        }
+
+        private string BuildResultString(List<char[]> grid, int numRows)
+        {
+            var resultSb = new StringBuilder();
+
+            for (int i = 0; i < numRows; i++)
+            {
+                foreach (var bucket in grid)
+                {
+                    if (bucket[i] != '\0')
+                        resultSb.Append(bucket[i]);
+                }
+            }
+
+            return resultSb.ToString();
         }
     }
 }
--- Algorithms/Medium/Strings/6. ZigZag Conversion/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
+++ Algorithms/Medium/Strings/6. ZigZag Conversion/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -12,6 +12,9 @@ namespace ConsoleApp1.Tests
         [DataTestMethod()]
         [DataRow("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR")]
         [DataRow("PAYPALISHIRING", 4, "PINALSIGYAHRPI")]
+        [DataRow("AB", 1, "AB")]
+        [DataRow("ABC", 1, "ABC")]
+        [DataRow("ABCD", 2, "ACBD")]
         public void ConvertTest(string input, int n, string expected)
         {
             // arrange
