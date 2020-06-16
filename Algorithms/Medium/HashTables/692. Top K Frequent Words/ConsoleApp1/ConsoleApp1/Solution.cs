b09ff6c4c5fa83fcc4752fa042121cc5ef9c6964
and85 <andreytkach85@gmai.com>
2020-06-16 22:59:49 +0200

    692. Top K Frequent Words

---
+++ Algorithms/Medium/HashTables/692. Top K Frequent Words/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public IList<string> TopKFrequent(string[] words, int k)
+        {
+            var wordsCount = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (wordsCount.ContainsKey(word))
+                    wordsCount[word]++;
+                else
+                    wordsCount.Add(word, 1);
+            }
+
+            return wordsCount
+                    .OrderByDescending(x => x.Value)
+                    .ThenBy(x => x.Key)
+                    .Take(k)
+                    .Select(x => x.Key)
+                    .ToList();
+        }
+    }
+}
