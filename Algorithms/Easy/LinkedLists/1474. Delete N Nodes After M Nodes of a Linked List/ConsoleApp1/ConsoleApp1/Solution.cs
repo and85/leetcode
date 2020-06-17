0e85fb2fe3e465d670ec0a58b1d5995cc9eb91b3
and85 <andreytkach85@gmai.com>
2020-06-17 23:35:24 +0200

    1474. Delete N Nodes After M Nodes of a Linked List

---
+++ Algorithms/Easy/LinkedLists/1474. Delete N Nodes After M Nodes of a Linked List/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public ListNode DeleteNodes(ListNode head, int m, int n)
+        {
+            ListNode curr = head;
+
+            int keepCounter = 0, removeCounter = 0;
+            while (curr != null)
+            {
+                keepCounter++;
+
+                if (keepCounter == m)
+                {
+                    while (removeCounter++ != n)
+                    {
+                        curr.next = curr.next?.next;
+                    }
+
+                    keepCounter = 0;
+                    removeCounter = 0;
+                }
+
+                curr = curr.next;
+            }
+
+            return head;
+        }
+    }
+}
