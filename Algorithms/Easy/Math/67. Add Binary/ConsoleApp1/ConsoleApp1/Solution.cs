ac349d286fd441d4196eb8162968f27986ee5485
Andrii.Tkach <andreytkach85@gmail.com>
2020-06-19 00:12:08 +0200

    67. Add Binary

---
+++ Algorithms/Easy/Math/67. Add Binary/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public string AddBinary(string a, string b)
+        {
+            int currValue, currDigit, carry = 0;
+            string aDigit, bDigit;
+
+            int i = a.Length - 1;
+            int j = b.Length - 1;
+
+            StringBuilder revertedResult = new StringBuilder();
+            while (i >=0 || j >= 0)
+            {
+                aDigit = (i >= 0) ? a[i].ToString() : "0";
+                bDigit = (j >= 0) ? b[j].ToString() : "0";
+
+                currValue = Convert.ToInt32(aDigit) + Convert.ToInt32(bDigit) + carry;
+
+                currDigit = currValue % 2;
+                carry = currValue / 2;
+
+                revertedResult.Append(currDigit);
+
+                i--;
+                j--;
+            }
+
+            if (carry == 1)
+                revertedResult.Append(1);
+
+            var arr = revertedResult.ToString().ToCharArray();
+            Array.Reverse(arr);
+
+            return new string(arr);
+        }
+    }
+}
---
+++ Algorithms/Easy/Math/67. Add Binary/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod()]
+        [DataRow("11", "1", "100")]
+        public void AddBinaryTest(string a, string b, string expected)
+        {
+            // arrange
+
+            // act
+            var actual = new Solution().AddBinary(a, b);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
