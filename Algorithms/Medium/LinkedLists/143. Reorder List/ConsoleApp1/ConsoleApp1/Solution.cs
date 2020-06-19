e4e0755411a002238bd440dbeb54b33ff2071d99
Andrii.Tkach <andreytkach85@gmail.com>
2020-06-19 23:52:34 +0200

    143. Reorder List

---
+++ Algorithms/Medium/LinkedLists/143. Reorder List/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        //Given a singly linked list L: L0→L1→…→Ln-1→Ln,
+        //reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
+        public void ReorderList(ListNode head)
+        {
+            if (head == null)
+                return;
+
+            Stack<ListNode> reversedNodes = GetReversedNodes(head);
+            int median = reversedNodes.Count / 2;
+            int i = 0;
+            ListNode curr = head;
+            ListNode reversedNode;
+            while (i < median)
+            {
+                reversedNode = reversedNodes.Pop();
+                reversedNode.next = curr.next;
+                curr.next = reversedNode;
+                curr = reversedNode.next;
+                i++;
+            }
+
+            // get rid of infinitive loop
+            curr.next = null;
+        }
+
+        private static Stack<ListNode> GetReversedNodes(ListNode head)
+        {
+            var reversedNode = new Stack<ListNode>();
+
+            ListNode curr = head;
+            while (curr != null)
+            {
+                reversedNode.Push(curr);
+                curr = curr.next;
+            }
+
+            return reversedNode;
+        }
+    }
+}
---
+++ Algorithms/Medium/LinkedLists/143. Reorder List/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void ReorderListTest()
+        {
+            // arrange
+            var head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(4);
+
+            // act
+            new Solution().ReorderList(head);
+
+            // assert
+
+        }
+    }
+}
