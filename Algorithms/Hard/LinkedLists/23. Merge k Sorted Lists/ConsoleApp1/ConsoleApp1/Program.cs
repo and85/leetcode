b10ee21e8601e13b65893836e71fddacbd403356
and85 <andreytkach85@gmai.com>
2020-06-23 23:16:39 +0200

    23. Merge K sorted lists

---
+++ Algorithms/Hard/LinkedLists/23. Merge k Sorted Lists/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var l1 = new ListNode(1);
+            l1.next = new ListNode(4);
+            l1.next.next = new ListNode(5);
+
+            var l2 = new ListNode(1);
+            l2.next = new ListNode(3);
+            l2.next.next = new ListNode(4);
+
+            var l3 = new ListNode(2);
+            l3.next = new ListNode(6);
+
+            var solution = new Solution();
+            var list = new ListNode[3] { l1, l2, l3 };
+            solution.MergeKLists(list);
+        }
+    }
+}
---
+++ Algorithms/Hard/LinkedLists/23. Merge k Sorted Lists/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            int k = lists.Length;
+            var resultDummyHead = new ListNode();
+            var curr = resultDummyHead; 
+
+            while (!AllPointersAreNull(lists))
+            {
+                int minListIndex = GetMinListIndex(lists);
+                curr.next = new ListNode();
+                curr = curr.next;
+                curr.val = lists[minListIndex].val;
+
+                lists[minListIndex] = lists[minListIndex].next;
+            }
+
+            return resultDummyHead.next;
+        }
+
+        private int GetMinListIndex(ListNode[] lists)
+        {
+            int min = int.MaxValue;
+            int minIndex = int.MaxValue;
+
+            for (int i = 0; i < lists.Length; i++)
+            {
+                if ((lists[i]?.val ?? int.MaxValue) < min)
+                {
+                    min = lists[i].val;
+                    minIndex = i;
+                }
+            }
+
+            return minIndex;
+        }
+
+        private bool AllPointersAreNull(ListNode[] lists)
+        {
+            return lists.All(l => l == null);
+        }
+    }
+}
