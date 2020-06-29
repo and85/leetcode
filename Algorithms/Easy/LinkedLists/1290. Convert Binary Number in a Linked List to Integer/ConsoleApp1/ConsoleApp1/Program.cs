a19d6d2603302d5df65cfb3aa145c1d29735648e
Andrii.Tkach <andreytkach85@gmail.com>
2020-06-30 00:05:14 +0200

    1290. Convert Binary Number in a Linked List to Integer

---
+++ Algorithms/Easy/LinkedLists/1290. Convert Binary Number in a Linked List to Integer/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+            var solution = new Solution();
+            solution.GetDecimalValue(head);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/Easy/LinkedLists/1290. Convert Binary Number in a Linked List to Integer/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int GetDecimalValue(ListNode head)
+        {
+            ListNode reversed = ReverseList(head);
+
+            int i = 0, result = 0;
+            ListNode curr = reversed;
+
+            while (curr != null)
+            {
+                result += curr.val == 1 ? (int)Math.Pow(2, i) : 0; 
+                i++;
+                curr = curr.next;
+            }
+
+            return result;
+        }
+
+        private ListNode ReverseList(ListNode head)
+        {
+            ListNode curr = head, next, prev = null;
+
+            while (curr != null)
+            {
+                next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+            return prev;
+        }
+    }
+}
