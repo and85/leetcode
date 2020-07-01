765864792c0b8827fad229f49ebbb5f4217e518d
Andrii.Tkach <andreytkach85@gmail.com>
2020-07-01 23:12:30 +0200

    938. Range Sum of BST

---
+++ Algorithms/Easy/Recursion/938. Range Sum of BST/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(10);
+            TreeNode l1 = new TreeNode(5);
+            TreeNode r1 = new TreeNode(15);
+
+            TreeNode l2 = new TreeNode(3);
+            TreeNode r2 = new TreeNode(7);
+            TreeNode r3 = new TreeNode(18);
+
+            root.left = l1;
+            root.right = r1;
+            l1.left = l2;
+            l1.right = r2;
+            r1.right = r3;
+
+            var solution = new Solution();
+            int a = solution.RangeSumBST(root, 7, 15);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/Easy/Recursion/938. Range Sum of BST/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        int _sum = 0;
+        public int RangeSumBST(TreeNode root, int L, int R)
+        {
+            if (root == null)
+                return 0;
+
+            _sum += (root.val >= L && root.val <= R) ? root.val : 0;
+            //int sum = root.val;
+
+            //int leftValue = root.left?.val ?? 0;
+            //int rightValue = root.right?.val ?? 0;
+
+            //if (leftValue >= L && leftValue <= R)
+            {
+                RangeSumBST(root.left, L, R);
+            }
+
+            //if (rightValue >= L && rightValue <= R)
+            {
+                RangeSumBST(root.right, L, R);
+            }
+
+            return _sum;
+        }
+    }
+}
