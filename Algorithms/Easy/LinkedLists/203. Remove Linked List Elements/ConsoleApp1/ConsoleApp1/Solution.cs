2f0bfd95c937c7f5cde5ceb83b534df330b59f3a
Andrii.Tkach <andreytkach85@gmail.com>
2020-07-01 18:35:06 +0200

    203. Remove Linked List Elements

--- Algorithms/Easy/LinkedLists/203. Remove Linked List Elements/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Easy/LinkedLists/203. Remove Linked List Elements/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -8,6 +8,17 @@ namespace ConsoleApp1
     {
         public ListNode RemoveElements(ListNode head, int val)
         {
+            ListNode sentinel = new ListNode(0);
+            sentinel.next = head;
+
+            ListNode prev = sentinel, curr = head;
+            while (curr != null)
+            {
+                if (curr.val == val) prev.next = curr.next;
+                else prev = curr;
+                curr = curr.next;
+            }
+            return sentinel.next;
         }
     }
 }
