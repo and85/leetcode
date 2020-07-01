7ef5c477adc551c0791bbef26c339e61c3c432be
Andrii.Tkach <andreytkach85@gmail.com>
2020-07-01 14:12:56 +0200

    1290. Convert Binary Number in a Linked List to Integer

---
+++ Algorithms/Medium/Queue/582. Kill Process/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        private Queue<int> _killedProcesses = new Queue<int>();
+        private Dictionary<int, List<int>> _processTree = new Dictionary<int, List<int>>();
+
+        public IList<int> KillProcess(IList<int> pid, IList<int> ppid, int kill)
+        {
+            var pids = pid.ToArray<int>();
+            var ppids = ppid.ToArray<int>();
+
+            for (int i = 0; i < ppids.Length; i++)
+            {
+                if (!_processTree.ContainsKey(ppids[i]))
+                {
+                    _processTree.Add(ppids[i], new List<int>() { pid[i]});
+                }
+                else
+                {
+                    _processTree[ppids[i]].Add(pid[i]);
+                }
+            }
+
+            return Kill(kill);
+        }
+
+        //private IList<int> Kill(int kill)
+        //{
+        //    _killedProcesses.Enqueue(kill);
+
+        //    if (_processTree.ContainsKey(kill))
+        //    {
+        //        List<int> childs = _processTree[kill];
+        //        foreach (int child in childs)
+        //        {
+        //            //_killedProcesses.Enqueue(child);
+        //            Kill(child);
+        //        }
+        //    }
+
+        //    return _killedProcesses
+        //        //.Distinct<int>()
+        //        .ToList();
+        //}
+
+        private IList<int> Kill(int kill)
+        {
+            List<int> res = new List<int>();
+            _killedProcesses.Enqueue(kill);
+            res.Add(kill);
+
+            while (_killedProcesses.Count != 0)
+            {
+                int process = _killedProcesses.Dequeue();
+                if (_processTree.ContainsKey(process))
+                {
+                    List<int> childs = _processTree[process];
+                    foreach (int child in childs)
+                    {
+                        _killedProcesses.Enqueue(child);
+                        res.Add(child);
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Algorithms/Medium/Queue/582. Kill Process/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void KillProcessTest1()
+        {
+            // arrange
+            var pid = new int[] { 1, 3, 10, 5 };
+            var ppid = new int[] { 3, 0, 5, 3 };
+            int kill = 5;
+            var solution = new Solution();
+
+            // act
+            var actual = solution.KillProcess(pid, ppid, kill).ToArray<int>();
+
+            // assert
+            Assert.AreEqual(2, actual.Length);
+            Assert.AreEqual(5, actual[0]);
+            Assert.AreEqual(10, actual[1]);
+        }
+
+        [TestMethod()]
+        public void KillProcessTest2()
+        {
+            // arrange
+            var pid = new int[] { 1, 2, 3 };
+            var ppid = new int[] { 0, 1, 2 };
+            int kill = 1;
+            var solution = new Solution();
+
+            // act
+            var actual = solution.KillProcess(pid, ppid, kill).ToArray<int>();
+
+            // assert
+            Assert.AreEqual(3, actual.Length);
+            Assert.AreEqual(1, actual[0]);
+            Assert.AreEqual(2, actual[1]);
+            Assert.AreEqual(3, actual[2]);
+        }
+    }
+}
