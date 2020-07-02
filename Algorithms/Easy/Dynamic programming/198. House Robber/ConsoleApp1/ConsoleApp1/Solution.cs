60f1b5322b48d1fab097fe820c145f641528064d
Andrii.Tkach <andreytkach85@gmail.com>
2020-07-02 22:38:42 +0200

    198. House Robber

---
+++ Algorithms/Easy/Dynamic programming/198. House Robber/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int Rob(int[] nums)
+        {
+            int prevMax = 0;
+            int curMax = 0;
+
+            foreach (int n in nums)
+            {
+                int temp = curMax;
+                curMax = Math.Max(prevMax + n, curMax);
+                prevMax = temp;
+            }
+
+            return curMax;
+        }
+    }
+}
---
+++ Algorithms/Easy/Dynamic programming/198. House Robber/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod()]
+        [DataRow(new int[] { 1, 2, 3, 1 }, 4)]
+        [DataRow(new int[] { 2, 7, 9, 3, 1 }, 12)]
+        public void RobTest(int[] input, int expected)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            int actual = solution.Rob(input);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
