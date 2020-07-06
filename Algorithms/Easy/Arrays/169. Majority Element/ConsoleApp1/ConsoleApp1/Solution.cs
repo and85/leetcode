f0d69c4082495c9527131f8cd1c8efd2e9a2ebf7
Andrii.Tkach <andreytkach85@gmail.com>
2020-07-06 23:11:23 +0200

    169. Majority Element

---
+++ Algorithms/Easy/Arrays/169. Majority Element/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            return nums.GroupBy(x => x)
+                .ToDictionary(x => x.Key, x => x.Count())
+                .Where(x => x.Value > nums.Length / 2)
+                .First()
+                .Key;
+            
+        }
+    }
+}
---
+++ Algorithms/Easy/LinkedLists/234. Palindrome Linked List/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+
+        }
+    }
+}
