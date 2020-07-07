bf3e9a58008e4e598b7614f14d0928377261e377
Andrii.Tkach <andreytkach85@gmail.com>
2020-07-08 00:10:49 +0200

    14. Longest commoinm prefix

---
+++ Algorithms/Easy/Strings/14. Longest Common Prefix/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            string[] array = new string[] { "flower", "flow", "flight" };
+            solution.LongestCommonPrefix(array);
+        }
+    }
+}
---
+++ Algorithms/Easy/Strings/14. Longest Common Prefix/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0)
+                return string.Empty;
+
+            if (strs.Length == 1)
+                return strs[0];
+
+            StringBuilder prefix = new StringBuilder();
+            int shortestLenght = strs.Min(s => s.Length);
+            bool breakFlag = false;
+
+            for (int i = 0; i < shortestLenght; i++)
+            {
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (strs[j - 1][i] != strs[j][i])
+                    {
+                        breakFlag = true;
+                        break;
+                    }
+                }
+
+                if (breakFlag) break;
+                prefix.Append(strs[0][i]);
+            }
+
+            return prefix.ToString();
+        }
+    }
+}
