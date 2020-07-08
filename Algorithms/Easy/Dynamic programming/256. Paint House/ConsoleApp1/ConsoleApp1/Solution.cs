c335e59b89f3317507db79750f8e7997a346d08b
Andrii.Tkach <andreytkach85@gmail.com>
2020-07-09 00:05:54 +0200

    933. Number of Recent calls

---
+++ Algorithms/Easy/Dynamic programming/256. Paint House/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int MinCost(int[][] costs)
+        {
+            // TODO: this problem is partially similar to house rober
+            int priceCandidate = int.MaxValue;
+
+            for (int i = 1; i < costs.Length; i++)
+            {
+                int price = GetMinPrice(costs[i - 1], );
+                if (priceCandidate > price)
+                    priceCandidate = price;
+            }
+
+            return priceCandidate;
+        }
+
+        private int GetMinPrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Algorithms/Easy/Queues/933. Number of Recent Calls/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class RecentCounter
+    {
+
+        Queue<int> _queue;
+        public RecentCounter()
+        {
+            _queue = new Queue<int>();
+        }
+
+        public int Ping(int t)
+        {
+            _queue.Enqueue(t);
+            while (_queue.Peek() < t - 3000)
+                _queue.Dequeue();
+            return _queue.Count;
+        }
+    }
+}
