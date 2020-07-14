c865c51b9d32bc13425d708d9ab729200f2a140d
and85 <andreytkach85@gmai.com>
2020-07-15 00:05:37 +0200

    383. Ransom Note

---
+++ Algorithms/Easy/Strings/383. Ransom Note/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            if (ransomNote.Length > magazine.Length)
+                return false;
+
+            var magazineLetters = magazine.GroupBy(c => c).ToDictionary(c => c.Key, c => c.Count());
+            var ransomeLetters = ransomNote.GroupBy(c => c).ToDictionary(c => c.Key, c => c.Count());
+
+            return ransomeLetters.All(r => magazineLetters.ContainsKey(r.Key) && r.Value <= magazineLetters[r.Key]);
+        }
+    }
+}
