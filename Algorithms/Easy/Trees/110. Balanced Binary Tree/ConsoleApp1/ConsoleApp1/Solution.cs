6b5f0452863c77c25cd485b1c72b2f401f3d5ed6
Andrii.Tkach <andreytkach85@gmail.com>
2020-07-17 00:02:55 +0200

    110. Balanced Tree

---
+++ Algorithms/Easy/Trees/110. Balanced Binary Tree/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public bool IsBalanced(TreeNode root)
+        {
+            if (root == null || (root.left == null && root.right == null))
+                return true;
+
+            return Math.Abs(GetHeight(root.left) - GetHeight(root.right)) <= 1
+                && IsBalanced(root.left)
+                && IsBalanced(root.right);
+        }
+
+        private int GetHeight(TreeNode root)
+        {
+            if (root == null)
+                return -1;
+
+            return Math.Max(GetHeight(root.left), GetHeight(root.right)) + 1;
+        }
+    }
+
+}
