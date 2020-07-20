f99e5aad4e66954e6cc1abf91626ebe839ce2b0c
and85 <andreytkach85@gmai.com>
2020-07-21 00:16:14 +0200

    48. Rotate Image

---
+++ Algorithms/Medium/Array/48. Rotate Image/ConsoleApp4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            int[][] matrix = new int[][]
+            {
+                new int[] { 5, 1, 9, 11 },
+                new int[] { 2, 4, 8, 10 },
+                new int[] { 13, 3, 6, 7 },
+                new int[] { 15,14,12,16 }
+            };
+
+            PrintMatrix(matrix);
+
+            solution.Rotate(matrix);
+
+            Console.WriteLine("--------------------------");
+            PrintMatrix(matrix);
+        }
+
+        private static void PrintMatrix(int[][] matrix)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix.Length; j++)
+                {
+                    Console.Write(matrix[i][j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Algorithms/Medium/Array/48. Rotate Image/ConsoleApp4/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Solution
+    {
+        public void Rotate(int[][] matrix)
+        {
+            int layersNumber = matrix.Length / 2;
+
+            for (int layerNumber = 0; layerNumber < layersNumber; layerNumber++)
+            {
+                RotateLayer(matrix, layerNumber);
+            }
+        }
+
+        private void RotateLayer(int[][] matrix, int layerNumber)
+        {
+            int first = layerNumber;
+            int last = matrix.Length - layerNumber - 1;
+
+            for (int i = first; i < last; i++)
+            {
+                int offset = i - first;
+                int temp = matrix[first][i];
+
+                matrix[first][i] = matrix[last - offset][first];
+                matrix[last - offset][first] = matrix[last][last - offset];
+                matrix[last][last - offset] = matrix[i][last];
+                matrix[i][last] = temp;
+            }
+        }
+    }
+}
