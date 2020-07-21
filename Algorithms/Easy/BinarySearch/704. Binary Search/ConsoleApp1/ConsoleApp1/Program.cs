f8e379a76d7fcbd1af347a96f81b8dd6d17735d5
Andrii.Tkach <andreytkach85@gmail.com>
2020-07-21 23:44:58 +0200

    704. Binary Search

---
+++ Algorithms/Easy/BinarySearch/704. Binary Search/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { -1, 0, 3, 5, 9, 12};
+
+            var solution = new Solution();
+            int res = solution.Search(nums, 9);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/Easy/BinarySearch/704. Binary Search/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int Search(int[] nums, int target)
+        {
+            int low = 0;
+            int high = nums.Length - 1;
+            int median;
+
+            while (low <= high)
+            {
+                median = low + (high - low) / 2;
+                if (nums[median] == target)
+                    return median;
+                else if (nums[median] < target)
+                    low = median + 1;
+                else if (nums[median] > target)
+                    high = median - 1;
+            }
+
+            return -1;
+        }
+    }
+}
