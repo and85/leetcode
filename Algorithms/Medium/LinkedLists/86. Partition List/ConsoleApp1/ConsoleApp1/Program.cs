280e77d2f4239053713b79eb96c95b93bbe01fba
Andrii.Tkach <andreytkach85@gmail.com>
2020-07-23 00:01:22 +0200

    86. Partition list

---
+++ Algorithms/Medium/LinkedLists/86. Partition List/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var node1 = new ListNode(1);
+            var node4 = new ListNode(4);
+            var node3 = new ListNode(3);
+            var node2 = new ListNode(2);
+            var node5 = new ListNode(5);
+            var node22 = new ListNode(2);
+            node1.next = node4;
+            node4.next = node3;
+            node3.next = node2;
+            node2.next = node5;
+            node5.next = node22;
+
+            var solution = new Solution();
+            solution.Partition(node1, 3);
+        }
+    }
+}
---
+++ Algorithms/Medium/LinkedLists/86. Partition List/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public ListNode Partition(ListNode head, int x)
+        {
+            ListNode lessNodesDummyHead = new ListNode();
+            ListNode greaterNodesDummyHead = new ListNode();
+            ListNode lessNodes = lessNodesDummyHead;
+            ListNode greaterOrEqualNodes = greaterNodesDummyHead;
+            ListNode curr = head;
+
+            while (curr != null)
+            {
+                var dummy = new ListNode(curr.val);
+
+                if (curr.val < x)
+                {
+                    lessNodes.next = dummy;
+                    lessNodes = lessNodes.next;
+                }
+                else
+                {
+                    greaterOrEqualNodes.next = dummy;
+                    greaterOrEqualNodes = greaterOrEqualNodes.next;
+                }
+
+                curr = curr.next;
+            }
+
+            lessNodes.next = greaterNodesDummyHead.next;
+
+            return lessNodesDummyHead.next;
+        }
+    }
+}
