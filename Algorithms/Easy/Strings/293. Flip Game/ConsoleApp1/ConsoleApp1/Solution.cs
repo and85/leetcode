119ce6e5576b2039ac9e60b3a9c1f2a511efbb6d
Andrii.Tkach <andreytkach85@gmail.com>
2020-07-28 23:55:12 +0200

    293. Flip Game

---
+++ Algorithms/Easy/Strings/293. Flip Game/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        /*
+        Input: s = "++++"
+        Output: 
+        [
+          "--++",
+          "+--+",
+          "++--"
+        ]
+        */
+        public IList<string> GeneratePossibleNextMoves(string s)
+        {
+            var result = new List<string>();
+            if (s.Length <= 1 || s == "--")
+                return result;
+            else if (s == "++")
+            {
+                result.Add("--");
+                return result;
+            }
+
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (s[i] != s[i + 1])
+                {
+                    continue;
+                }
+
+                if (s[i] == '+')
+                {
+                    char[] array = s.ToCharArray();
+                    array[i] = FlipChar(array[i]);
+                    array[i + 1] = array[i];
+                    result.Add(new string(array));
+                }
+            }
+
+            return result;
+        }
+
+        private char FlipChar(char c)
+        {
+            return '-';
+        }
+    }
+}
