08f34d1b90a224175bc488e306c358d1d9d917ea
Andrii.Tkach <andreytkach85@gmail.com>
2020-07-30 23:09:42 +0200

    500. Keyboard Row

---
+++ Algorithms/Easy/HashTable/500. Keyboard Row/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new string[] { "Hello", "Alaska", "Dad", "Peace" };
+            var solution = new Solution();
+            solution.FindWords(input);
+        }
+    }
+}
---
+++ Algorithms/Easy/HashTable/500. Keyboard Row/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public string[] FindWords(string[] words)
+        {
+            var result = new List<string>();
+            foreach (var word in words)
+            {
+                if (IsOneLineWord(word.ToLower()))
+                    result.Add(word);
+            }
+
+            return result.ToArray();
+
+        }
+
+        private bool IsOneLineWord(string word)
+        {
+            var row1 = new HashSet<char>() { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p' };
+            var row2 = new HashSet<char>() { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l' };
+            var row3 = new HashSet<char>() { 'z', 'x', 'c', 'v', 'b', 'n', 'm' };
+
+            var rowIndexes = new List<int>();
+            foreach (char ch in word)
+            {
+                int rowIndex = 0;
+                if (row1.Contains(ch))
+                    rowIndex = 1;
+                else if (row2.Contains(ch))
+                    rowIndex = 2;
+                else if (row3.Contains(ch))
+                    rowIndex = 3;
+                if (rowIndexes.Count != 0)
+                {
+                    if (!rowIndexes.Contains(rowIndex))
+                        return false;
+                }
+                else
+                    rowIndexes.Add(rowIndex);
+            }
+
+            return true;
+        }
+    }
+}
