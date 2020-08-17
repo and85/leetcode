f1ebd6d32095b78baa41a15085482d3bf60cff29
Andrii.Tkach <andreytkach85@gmail.com>
2020-08-17 21:14:43 +0200

    359. Logger Rate Limiter

---
+++ Algorithms/Easy/HashTable/359. Logger Rate Limiter/ConsoleApp1/ConsoleApp1/Logger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Logger
+    {
+        private Dictionary<string, int> _storage; 
+
+        public Logger()
+        {
+            _storage = new Dictionary<string, int>();
+        }
+
+        /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+        public bool ShouldPrintMessage(int timestamp, string message)
+        {
+            if (_storage.ContainsKey(message))
+            {
+                if (timestamp - _storage[message] < 10)
+                    return false;
+                else 
+                    _storage[message] = timestamp;
+            }
+            else
+            {
+                _storage.Add(message, timestamp);
+            }
+
+            return true;
+        }
+    }
+}
