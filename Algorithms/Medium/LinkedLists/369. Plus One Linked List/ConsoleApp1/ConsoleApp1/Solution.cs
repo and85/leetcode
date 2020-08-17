80121402f570403fe879dad651428624850a3372
Andrii.Tkach <andreytkach85@gmail.com>
2020-08-17 21:48:13 +0200

    369. Plus One Linked List

---
+++ Algorithms/Medium/LinkedLists/369. Plus One Linked List/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public ListNode PlusOne(ListNode head)
+        {
+            var stack = new Stack<ListNode>();
+            ListNode cur = head;
+            while (cur != null)
+            {
+                stack.Push(cur);
+                cur = cur.next;
+            }
+
+            int digit;
+            int carry = -1;
+
+            while (stack.Count != 0 && carry != 0)
+            {
+                digit = GetDigit(stack);
+                carry = digit / 10;
+            }
+            
+            if (carry == 1)
+            {
+                var newHead = new ListNode(1);
+                newHead.next = head;
+                head = newHead;
+            }
+
+            return head;
+        }
+
+        private static int GetDigit(Stack<ListNode> stack)
+        {
+            ListNode lastNode = stack.Pop();
+            int digit = lastNode.val + 1;
+            lastNode.val = digit % 10;
+            return digit;
+        }
+    }
+}
