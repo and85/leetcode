92b8b8e4792f54e588f78639c6acf735f4c5d53a
Andrii.Tkach <andreytkach85@gmail.com>
2020-08-26 21:10:50 +0200

    709. To Lower Case

---
+++ Algorithms/Easy/Strings/709. To Lower Case/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            solution.ToLowerCase("HelLo");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/Easy/Strings/709. To Lower Case/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public string ToLowerCase(string str)
+        {
+            var result = new StringBuilder();
+            for (int i = 0; i < str.Length; i++)
+            {
+                int index = str[i];
+                if (index >= 65 && index <= 90)
+                    result.Append(Convert.ToChar(index + 32));
+                else
+                    result.Append(str[i]);
+            }
+                
+            return result.ToString();
+        }
+    }
+}
