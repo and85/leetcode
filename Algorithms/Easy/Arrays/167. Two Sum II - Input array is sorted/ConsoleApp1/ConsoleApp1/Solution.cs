0baaff3e57130185c9e1608c0a98261edbd92153
Andrii.Tkach <andreytkach85@gmail.com>
2020-08-31 20:36:54 +0200

    167. Two Sum II - Input array is sorted

--- Algorithms/Easy/Arrays/167. Two Sum II - Input array is sorted/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Easy/Arrays/167. Two Sum II - Input array is sorted/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -8,6 +8,19 @@ namespace ConsoleApp1
     {
         public int[] TwoSum(int[] numbers, int target)
         {
+            var positions = new Dictionary<int, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (positions.ContainsKey(target - numbers[i]))
+                    return new[] { positions[target - numbers[i]], i + 1 };
+                else
+                {
+                    if (!positions.ContainsKey(numbers[i]))
+                        positions.Add(numbers[i], i + 1);
+                }
+            }
+
             return null;
         }
     }
