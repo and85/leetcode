e3416f8335062a691ee69b52e1821ce76bd1d294
Andrii.Tkach <andreytkach85@gmail.com>
2020-08-31 23:00:52 +0200

    520. Detect Capital

---
+++ Algorithms/Easy/Strings/520. Detect Capital/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public bool DetectCapitalUse(string word)
+        {
+            if (word.Length == 1)
+                return true;
+
+            if (word.All(l => char.IsUpper(l)))
+                return true;
+
+            if (word.All(l => char.IsLower(l)))
+                return true;
+
+            
+
+            string substring = word.Substring(1, word.Length - 1);
+            if (char.IsUpper(word[0]) && substring.All(l => char.IsLower(l)))
+                return true;
+
+            return false;
+        }
+    }
+}
--- Algorithms/Medium/Queue/621. Task Scheduler/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Medium/Queue/621. Task Scheduler/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ConsoleApp1
@@ -9,7 +10,60 @@ namespace ConsoleApp1
     {
         public int LeastInterval(char[] tasks, int n)
         {
-            return 0;
+            int leastInterval = 0;
+            char idle = '0';
+
+            Dictionary<char, int> taskEntrance = tasks.GroupBy(t => t)
+                .ToDictionary(g => g.Key, g => g.Count());
+
+            var lastProcessedTasks = new Queue<char>();
+            var leftTasks = new List<char>(tasks);
+
+            while (leftTasks.Count != 0)
+            {
+                bool taskDone = false;
+                foreach (var task in taskEntrance.ToList())
+                {
+                    if (CanExecute(task.Key, taskEntrance, lastProcessedTasks))
+                    {
+                        EnqueueTask(lastProcessedTasks, task.Key, n);
+                        taskDone = true;
+                        leastInterval += 1;
+                        leftTasks.Remove(task.Key);
+                        break;
+                    }
+                }
+
+                if (!taskDone)
+                {
+                    leastInterval += 1;
+                    EnqueueTask(lastProcessedTasks, idle, n);
+                }
+            }
+
+            return leastInterval;
+        }
+
+        private static void EnqueueTask(Queue<char> queue, char task, int capacity)
+        {
+            if (queue.Count == capacity && queue.Count > 0)
+            {
+                queue.Dequeue();
+            }
+
+            if (capacity > 0)
+                queue.Enqueue(task);
+        }
+
+        private bool CanExecute(char task, Dictionary<char, int> taskEntrance, Queue<char> queue)
+        {
+            if (taskEntrance.ContainsKey(task) && taskEntrance[task] > 0 && !queue.Contains(task))
+            {
+                taskEntrance[task]--;
+                return true;
+            }
+            return false;
+
         }
     }
 }
