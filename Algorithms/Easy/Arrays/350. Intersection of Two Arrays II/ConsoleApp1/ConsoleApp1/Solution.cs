3b17486a6ecf5267dd064936909069416dd5bb22
Andrii.Tkach <andreytkach85@gmail.com>
2020-09-02 19:51:19 +0200

    350. Intersection of Two Arrays II

---
+++ Algorithms/Easy/Arrays/350. Intersection of Two Arrays II/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            var dict1 = nums1.GroupBy(n => n).ToDictionary(g => g.Key, g => g.Count());
+            var dict2 = nums2.GroupBy(n => n).ToDictionary(g => g.Key, g => g.Count());
+            
+            foreach (var n in dict1.ToList())
+            {
+                if (dict2.ContainsKey(n.Key))
+                {
+                    dict1[n.Key] = Math.Min(dict1[n.Key], dict2[n.Key]);
+                }
+                else
+                    dict1.Remove(n.Key);
+            }
+
+            var resultList = new List<int>();
+            foreach (var n in dict1)
+            {
+                for (int i = 0; i < n.Value; i++)
+                    resultList.Add(n.Key);
+            }
+
+            return resultList.ToArray();
+        }
+    }
+}
---
+++ Algorithms/Easy/Arrays/350. Intersection of Two Arrays II/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod()]
+        [DataRow(new int[] { 1, 2, 2, 1 }, 
+            new int[] { 2, 2 }, 
+            new int[] { 2, 2 })]
+        [DataRow(new int[] { 4, 9, 5 },
+            new int[] { 9, 4, 9, 8, 4 },
+            new int[] { 4, 9 })]
+        public void IntersectTest1(int[] nums1, int[] nums2, int[] expected)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            int[] actual = solution.Intersect(nums1, nums2);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
