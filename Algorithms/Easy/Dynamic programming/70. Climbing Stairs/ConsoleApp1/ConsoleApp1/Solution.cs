572a11270786899aeba72eb229ceaafb28bfafd9
Andrii.Tkach <andreytkach85@gmail.com>
2020-09-03 18:28:34 +0200

    70. Climbing Stairs

---
+++ Algorithms/Easy/Dynamic programming/70. Climbing Stairs/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        private Dictionary<int, int> _memoization = new Dictionary<int, int>();
+
+        public int ClimbStairs(int n)
+        {
+            if (_memoization.ContainsKey(n))
+                return _memoization[n];
+
+            if (n < 0)
+                return 0;
+            
+            if (n == 0)
+                return 1;
+
+            int result = ClimbStairs(n - 1) + ClimbStairs(n - 2);
+            _memoization.Add(n, result);
+
+            return result;
+        }
+    }
+}
