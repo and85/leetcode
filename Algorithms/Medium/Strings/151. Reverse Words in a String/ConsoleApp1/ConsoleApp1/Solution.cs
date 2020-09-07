95da6b5dd18029dc3235cf6d30a27bdb7824984c
Andrii.Tkach <andreytkach85@gmail.com>
2020-09-07 22:19:52 +0200

    151. Reverse Words in a String

---
+++ Algorithms/Medium/Strings/151. Reverse Words in a String/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public string ReverseWords(string s)
+        {
+            string[] parts = s
+                .Split(' ')
+                .Where(p => !string.IsNullOrWhiteSpace(p))
+                .ToArray();
+            Array.Reverse(parts);
+
+            return string.Join(' ', parts);
+        }
+    }
+}
---
+++ Algorithms/Medium/Strings/151. Reverse Words in a String/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod()]
+        [DataRow("the sky is blue", "blue is sky the")]
+        [DataRow("  hello world!  ", "world! hello")]
+        [DataRow("a good   example", "example good a")]
+        public void ReverseWordsTest(string input, string expected)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            string actual = solution.ReverseWords(input);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
