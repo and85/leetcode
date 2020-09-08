635eca225197f5a9a785ef29024b3503e85a21c2
Andrii.Tkach <andreytkach85@gmail.com>
2020-09-08 18:46:43 +0200

    88. 88. Merge Sorted Array

--- Algorithms/Easy/Arrays/88. Merge Sorted Array/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Easy/Arrays/88. Merge Sorted Array/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -8,7 +8,19 @@ namespace ConsoleApp1
     {
         public void Merge(int[] nums1, int m, int[] nums2, int n)
         {
+            int p = m + n - 1, i = m - 1, j = n - 1;
             
+            while (i >= 0 || j >=0)
+            {
+                if (i < 0)
+                    nums1[p--] = nums2[j--];
+                else if (j < 0)
+                    nums1[p--] = nums1[i--];
+                else if (nums2[j] > nums1[i])
+                    nums1[p--] = nums2[j--];
+                else
+                    nums1[p--] = nums1[i--];
+            }
         }
 
         
