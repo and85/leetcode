c604bc9e124ff2a7638a18411e8b0cbb4f8834ec
Andrii.Tkach <andreytkach85@gmail.com>
2020-09-10 22:48:46 +0200

    1491. Average Salary Excluding the Minimum and Maximum Salary

---
+++ Algorithms/Easy/Arrays/1491. Average Salary Excluding the Minimum and Maximum Salary/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public double Average(int[] salary)
+        {
+            int minSalary = int.MaxValue;
+            int maxSalary = int.MinValue;
+            double totalSalary = 0;
+
+            for (int i = 0; i < salary.Length; i++)
+            {
+                if (salary[i] < minSalary)
+                    minSalary = salary[i];
+                if (salary[i] > maxSalary)
+                    maxSalary = salary[i];
+
+                totalSalary += salary[i];
+            }
+
+            return (totalSalary - minSalary - maxSalary) / (salary.Length - 2);
+        }
+    }
+}
---
+++ Algorithms/Medium/Design/1396. Design Underground System/ConsoleApp1/ConsoleApp1/UndergroundSystem.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    
+    public class UndergroundSystem
+    {
+        private Dictionary<string, Dictionary<int, int>> _checkIns;
+        private Dictionary<string, Dictionary<int, int>> _checkOuts;
+
+        public UndergroundSystem()
+        {
+            _checkIns = new Dictionary<string, Dictionary<int, int>>();
+            _checkOuts = new Dictionary<string, Dictionary<int, int>>();
+        }
+
+        public void CheckIn(int id, string stationName, int t)
+        {
+            RecordTravelTime(id, stationName, t, _checkIns);
+        }
+
+        // TODO: Update average travel time during checkouts,
+        // handle cases when the same pasanger can travel to the same station more than once
+        public void CheckOut(int id, string stationName, int t)
+        {
+            RecordTravelTime(id, stationName, t, _checkOuts);
+        }
+
+        public double GetAverageTime(string startStation, string endStation)
+        {
+            var checkIns = _checkIns[startStation];
+            var checkOuts = _checkOuts[endStation];
+
+            double totalTime = 0;
+            int journeyCounter = 0;
+
+            foreach (var checkOut in checkOuts)
+            {
+                int pasangerId = checkOut.Key;
+                if (checkIns.ContainsKey(pasangerId))
+                {
+                    totalTime += checkOut.Value - checkIns[pasangerId];
+                    journeyCounter++; 
+                }
+            }
+
+            return totalTime / journeyCounter;
+        }
+
+        private void RecordTravelTime(int id, string stationName, int t, Dictionary<string, Dictionary<int, int>> travelDictionary)
+        {
+            if (!travelDictionary.ContainsKey(stationName))
+            {
+                var checkIn = new Dictionary<int, int>()
+                {
+                    { id, t }
+                };
+                travelDictionary.Add(stationName, checkIn);
+            }
+            else
+            {
+                travelDictionary[stationName].Add(id, t);
+            }
+        }
+    }
+
+    /**
+     * Your UndergroundSystem object will be instantiated and called as such:
+     * UndergroundSystem obj = new UndergroundSystem();
+     * obj.CheckIn(id,stationName,t);
+     * obj.CheckOut(id,stationName,t);
+     * double param_3 = obj.GetAverageTime(startStation,endStation);
+     */
+}
