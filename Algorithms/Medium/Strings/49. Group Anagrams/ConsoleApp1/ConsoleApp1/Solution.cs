a5af31a016b8ead46ce65347089ff8fd47deb58c
Andrii.Tkach <andreytkach85@gmail.com>
2020-09-14 21:25:53 +0200

    49. Group anagrams

---
+++ Algorithms/Medium/Strings/49. Group Anagrams/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var result = new List<IList<string>>();
+            var grouped = new Dictionary<string, List<string>>();
+
+            foreach (string str in strs)
+            {
+                string sortedStr = GetSortedStr(str);
+                if (grouped.ContainsKey(sortedStr))
+                {
+                    grouped[sortedStr].Add(str);
+                }
+                else
+                {
+                    grouped.Add(sortedStr, new List<string>() { str });
+                }
+            }
+
+            foreach (var g in grouped)
+            {
+                result.Add(g.Value);
+            }
+            
+            return result;
+        }
+
+        private string GetSortedStr(string str)
+        {
+            var array = str.ToCharArray();
+            Array.Sort(array);
+
+            return new string(array);
+        }
+    }
+}
