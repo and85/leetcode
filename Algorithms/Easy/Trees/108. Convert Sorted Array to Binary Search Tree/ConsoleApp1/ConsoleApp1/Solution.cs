befb84f7ee04214d628cd04acf7b4465648bf0ee
Andrii.Tkach <andreytkach85@gmail.com>
2020-09-17 22:58:00 +0200

    108. Convert Sorted Array to Binary Search Tree

---
+++ Algorithms/Easy/Trees/108. Convert Sorted Array to Binary Search Tree/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+            return SortedArrayToBST(nums, 0, nums.Length - 1);
+        }
+
+        public TreeNode SortedArrayToBST(int[] arr, int start, int end)
+        {
+            if (start > end)
+            {
+                return null;
+            }
+
+            int mid = (start + end) / 2;
+            var node = new TreeNode(arr[mid]);
+
+            node.left = SortedArrayToBST(arr, start, mid - 1);
+            node.right = SortedArrayToBST(arr, mid + 1, end);
+
+            return node;
+        }
+    }
+}
