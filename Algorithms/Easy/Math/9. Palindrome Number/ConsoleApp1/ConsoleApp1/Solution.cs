09958cfcd65b3ed77a4fa94d02bd6bc92caf0d06
Andrii.Tkach <andreytkach85@gmail.com>
2020-09-17 23:16:30 +0200

    9. Palindrome number

---
+++ Algorithms/Easy/Math/9. Palindrome Number/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+                return false;
+
+            string originalString = x.ToString();
+            char[] digits = originalString.ToCharArray();
+            Array.Reverse(digits);
+            string reversedString = new string(digits);
+
+            return originalString.Equals(reversedString);
+        }
+    }
+}
