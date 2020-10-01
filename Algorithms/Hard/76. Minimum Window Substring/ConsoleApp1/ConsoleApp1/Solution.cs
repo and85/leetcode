65ef752f464d730298ff54141b535c1bcc76e51f
Andrii.Tkach <andreytkach85@gmail.com>
2020-10-01 23:26:51 +0200

    76. Minimum Window Substring. Time Limit Exceeded. Slightly improved

--- Algorithms/Hard/76. Minimum Window Substring/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Hard/76. Minimum Window Substring/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -16,6 +16,10 @@ namespace ConsoleApp1
             int firstPointer = 0, secondPointer = 0;
             int prevFirstPointer = -1, prevSecondPointer = -1;
             string candidate = string.Empty, shortestCandidate = s;
+            var candidateEntries = new Dictionary<char, int>();
+            bool removedIrelevant = false;
+            bool addedRemoved = false;
+            char removedSymbol = '0';
 
             while (firstPointer < s.Length && 
                 IsPointerMove(firstPointer, secondPointer, prevFirstPointer, prevSecondPointer))
@@ -30,27 +34,55 @@ namespace ConsoleApp1
 
                 // in the new candidate we should check not all entries, but only those that were
                 // removed when moving the window, otherwise it will be too many comparations!!! 
-                if (ContainsAllCharacters(candidate, tEntries))
+
+                if (removedIrelevant || addedRemoved || ContainsAllCharacters(candidateEntries, tEntries))
                 {
                     if (candidate.Length < shortestCandidate.Length)
                         shortestCandidate = candidate;
 
                     // remove from dictionary here
+                    char removedCh = s[firstPointer];
+                    if (tEntries.ContainsKey(removedCh))
+                    {
+                        candidateEntries[removedCh]--;
+                        removedIrelevant = false;
+                        removedSymbol = removedCh;
+                    }
+                    else
+                        removedIrelevant = true;
 
+                    addedRemoved = false;
                     firstPointer++;
                 }
                 else
                 {
                     // add to dictionary here
+                    if (secondPointer < s.Length)
+                    {
+                        char key = s[secondPointer];
+                        if (tEntries.ContainsKey(key))
+                        {
+                            addedRemoved = key == removedSymbol;
+                            if (candidateEntries.ContainsKey(key))
+                                candidateEntries[key]++;
+                            else
+                                candidateEntries.Add(key, 1);
+                        }
+                    }
 
                     secondPointer++;
+
                     // we can't move out of s string
                     if (secondPointer > s.Length)
                         secondPointer = s.Length;
                 }
             }
 
-            if (ContainsAllCharacters(shortestCandidate, tEntries))
+            Dictionary<char, int> shortestCandidateEntries = shortestCandidate.ToCharArray()
+                .GroupBy(c => c)
+                .ToDictionary(c => c.Key, c => c.Count());
+
+            if (ContainsAllCharacters(shortestCandidateEntries, tEntries))
                 return shortestCandidate;
 
             return string.Empty;
@@ -62,18 +94,10 @@ namespace ConsoleApp1
             return !(firstPointer == prevFirstPointer && secondPointer == prevSecondPointer);
         }
 
-        private bool ContainsAllCharacters(string candidate, Dictionary<char, int> tEntries)
+        private bool ContainsAllCharacters(Dictionary<char, int> candidate, 
+            Dictionary<char, int> tEntries)
         {
-            Dictionary<char, int> candidateEntries = candidate.ToCharArray()
-                .GroupBy(c => c)
-                .ToDictionary(c => c.Key, c => c.Count());
-
-            return AllInEntriesDictionary(candidateEntries, tEntries);
-        }
-
-        private bool AllInEntriesDictionary(Dictionary<char, int> candidate, Dictionary<char, int> entries)
-        {
-            foreach (var entry in entries)
+            foreach (var entry in tEntries)
             {
                 if (!candidate.ContainsKey(entry.Key) || candidate[entry.Key] < entry.Value)
                     return false;
