6fa5608943e60c043e9f4be7cc320ae514ea7aa3
Andrii.Tkach <andreytkach85@gmail.com>
2020-10-03 16:00:38 +0200

    56. Merge intervals not finished

--- Algorithms/Medium/Array/56. Merge Intervals/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Medium/Array/56. Merge Intervals/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -8,7 +8,31 @@ namespace ConsoleApp1
     {
         public int[][] Merge(int[][] intervals)
         {
+            if (intervals.Length <= 1)
+                return intervals;
 
+            var intervalStack = new Stack<int[]>();
+           
+            // todo: we should check in a loop if intervals overlap?
+
+            var merged = intervalStack.ToArray();
+            Array.Reverse(merged);
+
+            return merged;
+        }
+
+        private bool IntervalsOverlap(int[] interval1, int[] interval2, out int[] newInterval)
+        {
+            bool isOverlap = (interval1[1] >= interval2[0] && interval1[1] <= interval2[1]) ||
+                (interval2[1] >= interval1[0] && interval2[1] <= interval1[1]);
+           
+            newInterval = new int[2] 
+            { 
+                Math.Min(interval1[0], interval2[0]),
+                Math.Max(interval1[1], interval2[1]),
+            }; 
+
+            return isOverlap;
         }
     }
 }
---
+++ Algorithms/Medium/Array/56. Merge Intervals/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod()]
+        public void MergeTest1()
+        {
+            // arrange
+            int[][] intervals = new int[4][]
+            {
+                new int[] {1, 3 },
+                new int[] {2, 6 },
+                new int[] {8, 10 },
+                new int[] {15, 18 }
+            };
+            int[][] expected = new int[3][]
+            {
+                new int[] {1, 6 },
+                new int[] {8, 10 },
+                new int[] {15, 18 },
+            };
+
+            // act
+            var solution = new Solution();
+            var actual = solution.Merge(intervals);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual, StructuralComparisons.StructuralComparer);
+        }
+
+        [DataTestMethod()]
+        public void MergeTest2()
+        {
+            // arrange
+            int[][] intervals = new int[2][]
+            {
+                new int[] {1, 4 },
+                new int[] {4, 5 }
+            };
+            int[][] expected = new int[1][]
+            {
+                new int[] {1, 5 }
+            };
+
+            // act
+            var solution = new Solution();
+            var actual = solution.Merge(intervals);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual, StructuralComparisons.StructuralComparer);
+        }
+
+        [DataTestMethod()]
+        public void MergeTest3()
+        {
+            // arrange
+            int[][] intervals = new int[2][]
+            {
+                new int[] {1, 4 },
+                new int[] {0, 4 }
+            };
+            int[][] expected = new int[1][]
+            {
+                new int[] {0, 4 }
+            };
+
+            // act
+            var solution = new Solution();
+            var actual = solution.Merge(intervals);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual, StructuralComparisons.StructuralComparer);
+        }
+
+        [DataTestMethod()]
+        public void MergeTest4()
+        {
+            // arrange
+            int[][] intervals = new int[5][]
+            {
+                new int[] {2, 3 },
+                new int[] {4, 5 },
+                new int[] {6, 7 },
+                new int[] {8, 9 },
+                new int[] {1, 10 }
+
+            };
+            int[][] expected = new int[1][]
+            {
+                new int[] {1, 10 }
+            };
+
+            // act
+            var solution = new Solution();
+            var actual = solution.Merge(intervals);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual, StructuralComparisons.StructuralComparer);
+        }
+    }
+}
