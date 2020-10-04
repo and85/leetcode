18cb4ef82585b4ff00f30541d666d2eace2734e1
Andrii.Tkach <andreytkach85@gmail.com>
2020-10-04 23:13:08 +0200

    977. Squares of a Sorted Array

---
+++ Algorithms/Easy/Arrays/977. Squares of a Sorted Array/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int[] SortedSquares(int[] A)
+        {
+            for (int i = 0; i < A.Length; i++)
+                A[i] = (int)Math.Pow(A[i], 2);
+
+            Array.Sort(A);
+            return A;
+        }
+    }
+}
