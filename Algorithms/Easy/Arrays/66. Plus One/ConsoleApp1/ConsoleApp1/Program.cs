eaba2c043000f2743e0caf78f232c59dd087135c
Andrii.Tkach <andreytkach85@gmail.com>
2020-10-05 19:24:39 +0200

    66. Plus Once

---
+++ Algorithms/Easy/Arrays/66. Plus One/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] input = new int[1] { 9 };
+            int[] input = new int[4] { 8, 9, 9, 9 };
+
+            var solution = new Solution();
+            solution.PlusOne(input);
+
+        }
+    }
+}
---
+++ Algorithms/Easy/Arrays/66. Plus One/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            int lastIndex = digits.Length - 1;
+            int carry = (digits[lastIndex] + 1) / 10;
+            int prevCarry = carry;
+            digits[lastIndex] = (digits[lastIndex] + 1) % 10;
+
+            for (int i = digits.Length - 2; i >= 0; i--)
+            {
+                carry = (digits[i] + prevCarry) / 10;
+                digits[i] = (digits[i] + prevCarry) % 10;
+                prevCarry = carry;
+            }
+
+            if (carry == 0)
+                return digits;
+            else
+            {
+                int[] newDigits = new int[digits.Length + 1];
+                newDigits[0] = carry;
+                Array.Copy(digits, 0, newDigits, 1, digits.Length);
+                return newDigits;
+            }
+        }
+    }
+}
