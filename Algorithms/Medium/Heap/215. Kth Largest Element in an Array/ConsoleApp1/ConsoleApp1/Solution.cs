2e8a0771716aaf16f0b8ebb8f085b2f002ca85ce
Andrii.Tkach <andreytkach85@gmail.com>
2020-10-12 23:31:19 +0200

    215. Kth Largest Element in an Array. Naive

---
+++ Algorithms/Medium/Heap/215. Kth Largest Element in an Array/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int FindKthLargest(int[] nums, int k)
+        {
+            Array.Sort(nums, (int m, int n) => { return n - m; });
+            return nums[k - 1];
+        }
+    }
+}
