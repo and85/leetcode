08f03cf0ec630c7235d5954fca49efecb157d766
Andrii.Tkach <andreytkach85@gmail.com>
2020-10-12 22:55:46 +0200

    367. Valid Perfect Square

--- Algorithms/Easy/BinarySearch/367. Valid Perfect Square/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Easy/BinarySearch/367. Valid Perfect Square/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -8,6 +8,35 @@ namespace ConsoleApp1
     {
         public bool IsPerfectSquare(int num)
         {
+            if (num <= 1)
+                return true;
+
+            double high = num, low = 0, median = 0, epsilon = 0.000001;
+            while (high - low > epsilon)
+            {
+                median = (high - low) / 2 + low;
+
+                if (median * median - num > 0)
+                    high = median;
+                else if (median * median - num < 0)
+                    low = median;
+                else if (median * median - num == 0)
+                    return true;
+
+            }
+
+            return Round(median) * Round(median) == num;
+            //return Math.Round(median) * Math.Round(median) == num;
+        }
+
+        private int Round(double num)
+        {
+            if (num - (int)(num) >= 0.5)
+            {
+                return (int)(num) + 1;
+            }
+
+            return (int)(num);
         }
     }
 }
---
+++ Algorithms/Easy/BinarySearch/367. Valid Perfect Square/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod()]
+        [DataRow(16, true)]
+        [DataRow(14, false)]
+        [DataRow(9, true)]
+        [DataRow(6, false)]
+        [DataRow(5, false)]
+        [DataRow(7, false)]
+        [DataRow(64, true)]
+        [DataRow(2147395600, true)]
+        [DataRow(2147483647, false)]
+        public void IsPerfectSquareTest(int input, bool expected)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            bool actual = solution.IsPerfectSquare(input);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
