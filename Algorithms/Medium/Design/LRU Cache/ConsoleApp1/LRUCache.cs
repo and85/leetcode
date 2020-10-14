c9fca9df762a939ea85fbbc924e7f193e99150bb
Andrii.Tkach <andreytkach85@gmail.com>
2020-10-14 22:52:29 +0200

    LRU Cache

--- Algorithms/Medium/Design/LRU Cache/ConsoleApp1/LRUCache.cs
+++ Algorithms/Medium/Design/LRU Cache/ConsoleApp1/LRUCache.cs
@@ -8,20 +8,52 @@ namespace ConsoleApp1
 {
     public class LRUCache
     {
+        private Dictionary<int, int> _cache = new Dictionary<int, int>();
+        private Dictionary<int, int> _usage = new Dictionary<int, int>();
+
+        private int _capacity;
+        private int _usageCounter = 0;
 
         public LRUCache(int capacity)
         {
-
+            _capacity = capacity;
         }
 
         public int Get(int key)
         {
+            if (_cache.ContainsKey(key))
+            {
+                UpdateKeyUsage(key);
+                return _cache[key];
+            }
 
+            return -1;
         }
 
         public void Put(int key, int value)
         {
+            if (!_cache.ContainsKey(key) && _cache.Count == _capacity)
+            {
+                RemoveLeastRecentlyUsedElement();
+            }
 
+            UpdateKeyUsage(key);
+            _cache[key] = value;
+        }
+
+        private void RemoveLeastRecentlyUsedElement()
+        {
+            int leastUsage = _usage.Min(u => u.Value);
+            int leastKey = _usage.Single(u => u.Value == leastUsage).Key;
+
+            _usage.Remove(leastKey);
+            _cache.Remove(leastKey);
+        }
+
+        private void UpdateKeyUsage(int key)
+        {
+            _usageCounter++;
+            _usage[key] = _usageCounter;
         }
     }
 }
