fd56d29275fafa5c2c844baf1f661f332f77ad98
Andrii.Tkach <andreytkach85@gmail.com>
2020-10-15 23:53:03 +0200

    133. Clone Graph

---
+++ Algorithms/Medium/Graph/133. Clone Graph/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var node1 = new Node(1);
+            var node2 = new Node(2);
+            var node3 = new Node(3);
+            var node4 = new Node(4);
+
+            node1.neighbors.Add(node2);
+            node1.neighbors.Add(node4);
+
+            node2.neighbors.Add(node1);
+            node2.neighbors.Add(node3);
+
+            node3.neighbors.Add(node2);
+            node3.neighbors.Add(node4);
+
+            node4.neighbors.Add(node1);
+            node4.neighbors.Add(node3);
+
+            var solution = new Solution();
+            var clone = solution.CloneGraph(node1);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/Medium/Graph/133. Clone Graph/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        Dictionary<Node, Node> _visitedNodes = new Dictionary<Node, Node>();
+
+        public Node CloneGraph(Node node)
+        {
+            if (node == null)
+            {
+                return node;
+            }
+
+            if (_visitedNodes.ContainsKey(node))
+                return _visitedNodes[node];
+
+            Node clonedNode = new Node(node.val, new List<Node>());
+            _visitedNodes.Add(node, clonedNode);
+
+            foreach (var neighbor in node.neighbors)
+            {
+                clonedNode.neighbors.Add(CloneGraph(neighbor));
+            }
+
+            return clonedNode;
+        }
+    }
+}
