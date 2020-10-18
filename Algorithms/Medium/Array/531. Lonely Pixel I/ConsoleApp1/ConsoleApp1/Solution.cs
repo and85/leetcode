910b46fafb9b360085a245f1dab4bdf0120dfce0
Andrii.Tkach <andreytkach85@gmail.com>
2020-10-18 22:46:59 +0200

    531. Lonely Pixel I/ConsoleApp1/ConsoleApp1.sln

--- Algorithms/Medium/Array/531. Lonely Pixel I/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Medium/Array/531. Lonely Pixel I/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -1,14 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ConsoleApp1
 {
     public class Solution
     {
+        public struct Pixel
+        {
+            public Pixel(int row, int column)
+            {
+                Row = row;
+                Column = column;
+            }
+
+            public int Row { get; set; }
+            public int Column { get; set; }
+        }
+
         public int FindLonelyPixel(char[][] picture)
         {
+            int rowNum = picture.Length;
+            int colNum = picture[0].Length;
+            var rowEntries = new List<Pixel>[rowNum];
+            var colEntries = new List<Pixel>[colNum];
 
+            for (int r = 0; r < rowNum; r++)
+                for (int c = 0; c < colNum; c++)
+                {
+                    if (picture[r][c] == 'B')
+                    {
+                        if (rowEntries[r] == null)
+                            rowEntries[r] = new List<Pixel>();
+                        if (colEntries[c] == null)
+                            colEntries[c] = new List<Pixel>();
+
+
+                        rowEntries[r].Add(new Pixel(r, c));
+                        colEntries[c].Add(new Pixel(r, c));
+                    }
+                }
+
+            var rowList = rowEntries.Where(e => e?.Count == 1).Select(r => r[0]);
+            var colList = colEntries.Where(e => e?.Count == 1).Select(c => c[0]);
+
+            int count = rowList.Count(r =>
+                colList.Any(c => c.Row == r.Row
+                    && c.Column == r.Column));
+
+            return count;
         }
     }
 }
---
+++ Algorithms/Medium/Array/531. Lonely Pixel I/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void FindLonelyPixelTest1()
+        {
+            // arrange
+            char[][] input = new char[3][] 
+            {
+                new char[] { 'W', 'W', 'B'},
+                new char[] { 'W', 'B', 'W'},
+                new char[] { 'B', 'W', 'W'}
+            };
+
+            // act
+            var solution = new Solution();
+            var actual = solution.FindLonelyPixel(input);
+
+            // assert
+            Assert.AreEqual(3, actual);
+        }
+
+        [TestMethod()]
+        public void FindLonelyPixelTest2()
+        {
+            // arrange
+            char[][] input = new char[3][]
+            {
+                new char[] { 'W', 'W', 'W'},
+                new char[] { 'W', 'W', 'W'},
+                new char[] { 'W', 'W', 'W'}
+            };
+
+            // act
+            var solution = new Solution();
+            var actual = solution.FindLonelyPixel(input);
+
+            // assert
+            Assert.AreEqual(3, actual);
+        }
+
+        [TestMethod()]
+        public void FindLonelyPixelTest3()
+        {
+            // arrange
+            char[][] input = new char[1][]
+            {
+                new char[] { 'B', 'B', 'B'}
+            };
+
+            // act
+            var solution = new Solution();
+            var actual = solution.FindLonelyPixel(input);
+
+            // assert
+            Assert.AreEqual(0, actual);
+        }
+    }
+}
