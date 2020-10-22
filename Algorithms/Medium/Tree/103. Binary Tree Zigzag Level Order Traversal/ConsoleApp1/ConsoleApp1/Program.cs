af6aa0a6b87deb3a1f101fb0973e5caa5dcd6c0a
Andrii.Tkach <andreytkach85@gmail.com>
2020-10-22 21:13:04 +0200

    103. Binary Tree Zigzag

---
+++ Algorithms/Medium/Tree/103. Binary Tree Zigzag Level Order Traversal/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test2();
+
+            Console.ReadLine();
+
+        }
+
+        private static void Test1()
+        {
+            var node3 = new TreeNode(3);
+            var node9 = new TreeNode(9);
+            var node20 = new TreeNode(20);
+            var node15 = new TreeNode(15);
+            var node7 = new TreeNode(7);
+
+            node3.left = node9;
+            node3.right = node20;
+            node20.left = node15;
+            node20.right = node7;
+
+            var solution = new Solution();
+            solution.ZigzagLevelOrder(node3);
+        }
+
+        private static void Test2()
+        {
+            var node1 = new TreeNode(1);
+            var node2 = new TreeNode(2);
+            var node3 = new TreeNode(3);
+            var node4 = new TreeNode(4);
+            var node5 = new TreeNode(5);
+
+            node1.left = node2;
+            node2.left = node4;
+            node1.right = node3;
+            node3.right = node5;
+
+            var solution = new Solution();
+            solution.ZigzagLevelOrder(node1);
+        }
+    }
+}
---
+++ Algorithms/Medium/Tree/103. Binary Tree Zigzag Level Order Traversal/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+        {
+            var result = new List<IList<int>>();
+
+            if (root == null)
+                return result;
+            
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            bool leftRight = true;
+
+            while (queue.Count > 0)
+            {
+                int queueSize = queue.Count;
+                var curList = new List<int>();
+                for (int i = 0; i < queueSize; i++)
+                {
+                    var node = queue.Dequeue();
+                    curList.Add(node.val);
+
+                    PopulateQueue(node.left, queue);
+                    PopulateQueue(node.right, queue);
+                }
+
+                if (!leftRight)
+                {
+                    curList.Reverse();
+                }
+
+                leftRight = !leftRight;
+                result.Add(curList);
+            }
+
+            return result;
+        }
+
+        private void PopulateQueue(TreeNode node, 
+            Queue<TreeNode> stack)
+        {
+            if (node != null)
+            {
+                stack.Enqueue(node);
+            }
+        }
+    }
+}
