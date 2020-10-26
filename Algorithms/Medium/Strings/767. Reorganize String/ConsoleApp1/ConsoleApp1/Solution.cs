7e2bc116fcf578c49860beaaf633c3c18a63c822
Andrii.Tkach <andreytkach85@gmail.com>
2020-10-26 20:06:35 +0100

    767. Reorganize String

---
+++ Algorithms/Medium/Strings/767. Reorganize String/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public string ReorganizeString(string s)
+        {
+            var entrances = s.ToCharArray()
+                .GroupBy(c => c)
+                .ToDictionary(c => c.Key.ToString(), c => c.Count());
+
+            int maxCount = entrances.Max(e => e.Value);
+            if (IsPossible(entrances, maxCount, s.Length))
+            {
+                return ReorganizeString(entrances);
+            }
+
+            return string.Empty;    
+        }
+
+        private bool IsPossible(Dictionary<string, int> entrances, int maxCount, int length)
+        {
+            int restLength = length - maxCount;
+            return maxCount - restLength <= 1;
+        }
+
+        private string ReorganizeString(Dictionary<string, int> entrances)
+        {
+            var resultSb = new StringBuilder();
+            int maxCount = entrances.Max(e => e.Value);
+
+            while (maxCount > 0)
+            {
+                var maxEntrance = entrances.First(e => e.Value == maxCount);
+                string maxEntranceKey = maxEntrance.Key;
+
+                entrances[maxEntranceKey]--;
+                resultSb.Append(maxEntranceKey);
+
+                string otherKey = entrances
+                    .FirstOrDefault(e => e.Key != maxEntranceKey && e.Value > 0).Key;
+                if (otherKey != null)
+                {
+                    resultSb.Append(otherKey);
+                    entrances[otherKey]--;
+                }
+
+                maxCount = entrances.Max(e => e.Value);
+            }
+
+            return resultSb.ToString();
+        }
+    }
+}
---
+++ Algorithms/Medium/Strings/767. Reorganize String/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod()]
+        [DataRow("aab", "aba")]
+        [DataRow("aaab", "")]
+        public void ReorganizeStringTest(string input, string expected)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            string actual = solution.ReorganizeString(input);
+
+            // asset
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
