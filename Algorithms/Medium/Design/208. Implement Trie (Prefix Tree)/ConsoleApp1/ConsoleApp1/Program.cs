4b70f6b77d1fcfcce048ebce68d2632c4a5acc21
Andrii.Tkach <andreytkach85@gmail.com>
2020-10-27 22:50:54 +0100

    208. Implement Trie (Prefix Tree)

---
+++ Algorithms/Medium/Design/208. Implement Trie (Prefix Tree)/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test3();
+
+            Console.ReadLine();
+        }
+
+        private static void Test1()
+        {
+            Trie trie = new Trie();
+
+            trie.Insert("apple");
+            trie.Search("apple");   // returns true
+            trie.Search("app");     // returns false
+            trie.StartsWith("app"); // returns true
+            trie.Insert("app");
+            trie.Search("app");     // returns true
+        }
+
+        private static void Test2()
+        {
+            Trie trie = new Trie();
+            trie.Insert("app");
+            trie.Insert("apple");
+            trie.Insert("beer");
+            trie.Insert("add");
+            trie.Insert("jam");
+            trie.Insert("rental");
+            trie.Search("apps");
+            trie.Search("app");
+        }
+
+        private static void Test3()
+        {
+            Trie trie = new Trie();
+            trie.Insert("ab");
+            trie.Insert("ab");
+            trie.Insert("abc");
+            trie.Search("abc");
+            trie.StartsWith("abc");
+        }
+    }
+}
---
+++ Algorithms/Medium/Design/208. Implement Trie (Prefix Tree)/ConsoleApp1/ConsoleApp1/Trie.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Trie
+    {
+        public class TrieNode
+        {
+            public TrieNode()
+            {
+            }
+
+            public TrieNode(char value, bool isWordEnd)
+            {
+                Value = value;
+                IsWordEnd = isWordEnd;
+            }
+
+            public char Value { get; set; }
+            public List<TrieNode> Children { get; set; } = new List<TrieNode>();
+
+            public bool IsWordEnd { get; set; }
+        }
+
+        private TrieNode _root = new TrieNode();
+
+        /** Initialize your data structure here. */
+        public Trie()
+        {
+        }
+
+        /** Inserts a word into the trie. */
+        public void Insert(string word)
+        {
+            char letter;
+            TrieNode currNode = _root;
+            for (int i = 0; i < word.Length; i++)
+            {
+                letter = word[i];
+                var existingNode = currNode.Children.SingleOrDefault(c => c.Value == letter);
+                if (existingNode != null)
+                {
+                    if(!existingNode.IsWordEnd)
+                        existingNode.IsWordEnd = IsWordEnd(word, i);
+                    
+                    currNode = existingNode;
+                }
+                else
+                {
+                    var newNode = new TrieNode(letter, IsWordEnd(word, i));
+                    currNode.Children.Add(newNode);
+                    currNode = newNode;
+                }
+            }
+        }
+
+        /** Returns if the word is in the trie. */
+        public bool Search(string word)
+        {
+            return Search(word, false);
+        }
+
+        /** Returns if there is any word in the trie that starts with the given prefix. */
+        public bool StartsWith(string prefix)
+        {
+            return Search(prefix, true);
+        }
+
+        private bool Search(string word, bool startsWith)
+        {
+            char letter;
+            TrieNode currNode = _root;
+            for (int i = 0; i < word.Length; i++)
+            {
+                letter = word[i];
+                var existingNode = currNode.Children.SingleOrDefault(c => c.Value == letter);
+
+                if (existingNode == null)
+                    return false;
+
+                if (!startsWith && 
+                    (!existingNode.IsWordEnd && IsWordEnd(word, i)))
+                    return false;
+
+                currNode = existingNode;
+
+            }
+
+            return true;
+        }
+
+        private bool IsWordEnd(string word, int i)
+        {
+            return i == word.Length - 1;
+        }
+    }
+}
