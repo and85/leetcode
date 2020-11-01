c823cf3a316b19790985edbfc23bc74c978abae1
Andrii.Tkach <andreytkach85@gmail.com>
2020-11-01 18:28:12 +0100

    1480. Running Sum of 1d Array

---
+++ Algorithms/Easy/Arrays/1480. Running Sum of 1d Array/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] + nums[i - 1];
+            }
+
+            return nums;
+        }
+    }
+}
