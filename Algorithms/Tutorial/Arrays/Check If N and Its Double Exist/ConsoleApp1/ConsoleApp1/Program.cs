41cc722c571fedf4f133f1e59f6348332c7f5399
Andrii.Tkach <andreytkach85@gmail.com>
2020-11-03 23:26:21 +0100

    array turtorial

---
+++ Algorithms/Tutorial/Arrays/Check If N and Its Double Exist/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { -2, 0, 10, -19, 4, 6, -8 };
+
+            var solution = new Solution();
+            solution.CheckIfExist(arr);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/Tutorial/Arrays/Check If N and Its Double Exist/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public bool CheckIfExist(int[] arr)
+        {
+            // binary search would give O(nlog(n)) but would be in place if
+            // we sort an array and our sorting is in place, e.g. we use Quick sort
+
+            // O(n)
+            var hashTable = new HashSet<double>();
+            foreach (int el in arr)
+            {
+                if (hashTable.Contains(el * 2) || hashTable.Contains(el / 2.0))
+                    return true;
+
+                if (!hashTable.Contains(el))
+                    hashTable.Add(el);
+            }
+
+            return false;
+        }
+    }
+}
