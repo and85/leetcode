51cf437eab8a08392fa117c18b020a4c7977c825
Andrii.Tkach <andreytkach85@gmail.com>
2020-11-03 23:52:52 +0100

    Valid Mountain Array

---
+++ Algorithms/Tutorial/Arrays/Valid Mountain Array/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public bool ValidMountainArray(int[] a)
+        {
+            if (a.Length < 3)
+                return false;
+
+            bool isIncreasing = false;
+            bool isDecreasing = false;
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] == a[i - 1])
+                    return false;
+
+                if (a[i] > a[i - 1])
+                {
+                    if (isDecreasing)
+                        return false;
+
+                    isIncreasing = true;
+                }
+                else
+                {
+                    if (!isIncreasing)
+                        return false;
+
+                    isDecreasing = true;
+                }
+            }
+
+            return isIncreasing && isDecreasing;
+        }
+    }
+}
