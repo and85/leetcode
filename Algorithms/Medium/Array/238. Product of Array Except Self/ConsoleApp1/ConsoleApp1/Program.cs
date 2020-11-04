3827234a11263e44df721ab5d3463a77fb3f4344
Andrii.Tkach <andreytkach85@gmail.com>
2020-11-04 23:25:13 +0100

    238. Product of Array Except Self

---
+++ Algorithms/Medium/Array/238. Product of Array Except Self/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 1, -1 };
+
+            var solution = new Solution();
+            solution.ProductExceptSelf(input);
+
+            Console.ReadLine();
+        }
+    }
+}
