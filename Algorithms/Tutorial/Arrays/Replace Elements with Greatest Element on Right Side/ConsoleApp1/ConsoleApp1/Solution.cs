c8126db07c67e5a7eb198a60511c93078fa966e0
Andrii.Tkach <andreytkach85@gmail.com>
2020-11-04 21:36:24 +0100

    array tutorial

---
+++ Algorithms/Tutorial/Arrays/Replace Elements with Greatest Element on Right Side/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int[] ReplaceElements(int[] arr)
+        {
+            int maxSeen = -1;
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                int temp = arr[i];
+                arr[i] = maxSeen;
+                maxSeen = Math.Max(maxSeen, temp);
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ Algorithms/Tutorial/Arrays/Replace Elements with Greatest Element on Right Side/Sort Array By Parity/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int[] SortArrayByParity(int[] a)
+        {
+            int evenCounter = 0, temp;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 == 0)
+                {
+                    temp = a[evenCounter];
+                    a[evenCounter] = a[i];
+                    a[i] = temp;
+
+                    evenCounter++;
+                }
+            }
+
+            return a;
+        }
+    }
+}
