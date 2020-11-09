ed514659f25a37251180cc75866d82f34d06e457
Andrii.Tkach <andreytkach85@gmail.com>
2020-11-09 22:34:12 +0100

    MinCostClimbingStairs

---
+++ Algorithms/Easy/MinCostClimbingStairs/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            //int cost1 = solution.MinCostClimbingStairs(new int[] { 0, 1, 1, 1});
+            //int cost2 = solution.MinCostClimbingStairs(new int[] { 0, 2, 2, 1 });
+            //int cost3 = solution.MinCostClimbingStairs(new int[] { 0, 1, 2, 2 });
+            int cost4 = solution.MinCostClimbingStairs(new int[] { 1, 2, 3, 4, 5 });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/Easy/MinCostClimbingStairs/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int MinCostClimbingStairs(int[] cost)
+        {
+            int f1 = 0, f2 = 0;
+            for (int i = cost.Length - 1; i >= 0; --i)
+            {
+                int f0 = cost[i] + Math.Min(f1, f2);
+                f2 = f1;
+                f1 = f0;
+            }
+            return Math.Min(f1, f2);
+
+        }
+    }
+}
