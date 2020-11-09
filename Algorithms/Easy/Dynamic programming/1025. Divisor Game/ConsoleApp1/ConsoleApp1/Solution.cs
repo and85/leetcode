e99f7bb1e450212c5e8300a7020b05317b8f350a
Andrii.Tkach <andreytkach85@gmail.com>
2020-11-09 21:32:54 +0100

    1025. Divisor Game

---
+++ Algorithms/Easy/Dynamic programming/1025. Divisor Game/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+
+        Dictionary<int, bool> _memo = new Dictionary<int, bool>();
+
+        public bool DivisorGame(int N)
+        {
+            return N % 2 == 0;
+        }
+    }
+}
---
+++ Algorithms/Easy/Dynamic programming/1025. Divisor Game/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod()]
+        [DataRow(2, true)]
+        [DataRow(3, false)]
+        [DataRow(4, true)]
+        public void DivisorGameTest(int n, bool expected)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            bool actual = solution.DivisorGame(n);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
