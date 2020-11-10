d1031a673c9b256f2aa5d0eb18f2606f8835ab77
Andrii.Tkach <andreytkach85@gmail.com>
2020-11-10 22:20:26 +0100

    392. Is Subsequence

---
+++ Algorithms/Easy/Dynamic programming/392. Is Subsequence/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            int sIndex = 0;
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in t)
+            {
+                if (sIndex >= s.Length)
+                    break;
+
+                if (s[sIndex] == c)
+                {
+                    sb.Append(c);
+                    sIndex++;
+                }
+            }
+
+            return s.Equals(sb.ToString());
+        }
+    }
+}
---
+++ Algorithms/Easy/Dynamic programming/392. Is Subsequence/ConsoleApp1/ConsoleApp1Tests/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [DataTestMethod()]
+        [DataRow("abc", "ahbgdc", true)]
+        [DataRow("axc", "ahbgdc", false)]
+        public void IsSubsequenceTest(string s, string t, bool expected)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            bool actual = solution.IsSubsequence(s, t);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
