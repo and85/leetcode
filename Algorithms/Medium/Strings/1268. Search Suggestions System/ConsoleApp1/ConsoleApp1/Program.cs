51af09291027737bdf6fb8151e88dbaafb0cb0d1
Andrii.Tkach <andreytkach85@gmail.com>
2020-11-20 23:34:29 +0100

    1268. Search Suggestions System

---
+++ Algorithms/Medium/Strings/1268. Search Suggestions System/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var results1 = solution.SuggestedProducts(new string[] { "mobile", "mouse", "moneypot", "monitor", "mousepad" }, "mouse"); 
+            foreach (var line in results1)
+            {
+                Console.WriteLine(string.Join(" ", line));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/Medium/Strings/1268. Search Suggestions System/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public IList<IList<string>> SuggestedProducts(string[] products, string searchWord)
+        {
+            Array.Sort(products);
+            var result = new List<IList<string>>();
+            var matchedProducts = new List<string>(products);
+
+            var sb = new StringBuilder();
+            foreach (char letter in searchWord)
+            {
+                sb.Append(letter);
+                matchedProducts = matchedProducts.Where(p => p.StartsWith(sb.ToString())).ToList();
+                result.Add(matchedProducts.Take(3).ToList());
+            }
+
+            return result;
+        }
+    }
+}
