4e92425b547c763ed264b280bb73c67016cf5b1e
Andrii.Tkach <andreytkach85@gmail.com>
2020-11-24 11:21:30 +0100

    628. Maximum Product of Three Numbers

---
+++ Algorithms/Easy/Math/628. Maximum Product of Three Numbers/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            solution.MaximumProduct(new int[] { -6, 6, -5, 3, 4, 3, 4});
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/Easy/Math/628. Maximum Product of Three Numbers/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int MaximumProduct(int[] nums)
+        {
+            Array.Sort(nums);
+            int lastIndex = nums.Length - 1;
+
+            int candidate1 = nums[lastIndex] * nums[lastIndex - 1] * nums[lastIndex - 2];
+
+            int candidate2 = int.MinValue;
+            int minNegative1 = nums[0];
+            int minNegative2 = nums[1];
+            int maxValue = nums[lastIndex];
+            if (minNegative1 < 0 && minNegative2 < 0)
+            {
+                candidate2 = minNegative1 * minNegative2 * maxValue;
+            }
+
+            return Math.Max(candidate1, candidate2);
+        }
+    }
+}
