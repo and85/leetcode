6a1b0192ce6d1516a7b6e7dac833431d53e68a34
Andrii.Tkach <andreytkach85@gmail.com>
2020-11-24 12:05:46 +0100

    118. Pascal's Triangle

---
+++ Algorithms/Easy/Arrays/118. Pascal's Triangle/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            var result = new List<IList<int>>();
+            int[] prevRow = null;
+            for (int i = 0; i < numRows; i++)
+            {
+                var curRow = new List<int>();
+                if (prevRow == null)
+                {
+                    curRow.Add(1);
+                }
+                else
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        int num = 0;
+                        if (j - 1 < 0 || j >= prevRow.Length)
+                            num = 1;
+                        else
+                            num = prevRow[j - 1] + prevRow[j];
+                        curRow.Add(num);
+                    }
+                }
+
+                prevRow = curRow.ToArray();
+                result.Add(curRow);
+            }
+
+            return result;
+        }
+    }
+}
