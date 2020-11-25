2d6960014ad1df972c5a3c331d6004b9e703d670
Andrii.Tkach <andreytkach85@gmail.com>
2020-11-25 20:21:24 +0100

    50. Pow(x, n) and 1041. Robot Bounded

--- Algorithms/Medium/Math/1041. Robot Bounded In Circle/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Medium/Math/1041. Robot Bounded In Circle/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -8,9 +8,102 @@ namespace ConsoleApp1
 {
     public class Solution
     {
+        private const char Go = 'G';
+        private const char Left = 'L';
+        private const char Right = 'R';
+        private const int StepLength = 1;
+
+        private enum Direction
+        {
+            North,
+            South,
+            East,
+            West
+        }
+
+        private struct Coordinate
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
         public bool IsRobotBounded(string instructions)
         {
-            return false;
+            Coordinate curPosition = new Coordinate();
+            Direction curDirection = Direction.North;
+
+            foreach (char inst in instructions)
+            {
+                switch (inst)
+                {
+                    case Go:
+                        curPosition = Move(curPosition, curDirection);
+                        break;
+                    case Left:
+                    case Right:
+                        curDirection = Rotate(curDirection, inst);
+                        break;
+                }
+            }
+
+            return (IsStayedAtZeroPosition(curPosition))
+                || IsDirectionChanged(curDirection);
+        }
+
+        private bool IsStayedAtZeroPosition(Coordinate curPosition)
+        {
+            return curPosition.X == 0 && curPosition.Y == 0;
+        }
+
+        private bool IsDirectionChanged(Direction curDirection)
+        {
+            return curDirection != Direction.North;
+        }
+
+        private Direction Rotate(Direction curDirection, char inst)
+        {
+            switch (curDirection)
+            {
+                case Direction.North:
+                    if (inst.Equals(Left)) curDirection = Direction.West;
+                    if (inst.Equals(Right)) curDirection = Direction.East;
+                    break;
+                case Direction.South:
+                    if (inst.Equals(Left)) curDirection = Direction.East;
+                    if (inst.Equals(Right)) curDirection = Direction.West;
+                    break;
+                case Direction.East:
+                    if (inst.Equals(Left)) curDirection = Direction.North;
+                    if (inst.Equals(Right)) curDirection = Direction.South;
+                    break;
+                case Direction.West:
+                    if (inst.Equals(Left)) curDirection = Direction.South;
+                    if (inst.Equals(Right)) curDirection = Direction.North;
+                    break;
+            }
+
+            return curDirection;
+        }
+
+        private Coordinate Move(Coordinate curPosition, Direction curDirection)
+        {
+            switch (curDirection)
+            {
+                case Direction.North:
+                    curPosition.Y += StepLength;
+                    break;
+                case Direction.South:
+                    curPosition.Y -= StepLength;
+                    break;
+                case Direction.East:
+                    curPosition.X += StepLength;
+                    break;
+                case Direction.West:
+                    curPosition.X -= StepLength;
+                    break;
+            }
+
+            return curPosition;
         }
     }
 }
---
+++ Algorithms/Medium/Math/50. Pow(x, n)/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.MyPow(2, 4));
+            Console.WriteLine(solution.MyPow(2, -2));
+            Console.WriteLine(solution.MyPow(2, -4));
+            Console.WriteLine(solution.MyPow(3, 3));
+            Console.WriteLine(solution.MyPow(2, 5));
+            Console.WriteLine(solution.MyPow(2, 6));
+
+            Console.WriteLine(solution.MyPow(53, 1));
+            Console.WriteLine(solution.MyPow(53, 54));
+            Console.WriteLine(solution.MyPow(2.00000, -2147483648));
+
+
+            Console.ReadLine();
+        }
+    }
+}
