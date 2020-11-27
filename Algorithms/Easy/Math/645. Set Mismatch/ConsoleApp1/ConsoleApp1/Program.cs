ba1d63fbf3a94cf72eb3164577284c9f64cbe368
Andrii.Tkach <andreytkach85@gmail.com>
2020-11-27 22:32:56 +0100

    645. Set Mismatch

---
+++ Algorithms/Easy/Math/645. Set Mismatch/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1();
+            Test2();
+            Test3();
+
+            Console.ReadLine();
+        }
+
+        private static void Test1()
+        {
+            var solution = new Solution();
+            int[] nums = new int[] { 1, 2, 2, 4 };
+            var output = solution.FindErrorNums(nums);
+            foreach (int o in output)
+                Console.WriteLine(o);
+        }
+
+        private static void Test2()
+        {
+            var solution = new Solution();
+            int[] nums = new int[] { 1, 1 };
+            var output = solution.FindErrorNums(nums);
+            foreach (int o in output)
+                Console.WriteLine(o);
+        }
+
+        private static void Test3()
+        {
+            var solution = new Solution();
+            int[] nums = new int[] { 2, 2 };
+            var output = solution.FindErrorNums(nums);
+            foreach (int o in output)
+                Console.WriteLine(o);
+        }
+    }
+}
---
+++ Algorithms/Easy/Math/645. Set Mismatch/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int[] FindErrorNums(int[] nums)
+        {
+            var dict = new Dictionary<int, int>();
+            for (int i = 1; i <= nums.Length; i++)
+                dict.Add(i, 0);
+
+            for (int i = 0; i < nums.Length; i++)
+                dict[nums[i]]++;
+
+            return new int[]
+            {
+                dict.Single(num => num.Value == 2).Key,
+                dict.Single(num => num.Value == 0).Key
+            };
+        }
+    }
+}
