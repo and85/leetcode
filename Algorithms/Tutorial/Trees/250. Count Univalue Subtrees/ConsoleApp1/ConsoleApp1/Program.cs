7115c2557563c4578d2425d8110b114fef7d8352
Andrii.Tkach <andreytkach85@gmail.com>
2020-12-02 22:56:39 +0100

    250. Count Univalue Subtrees

---
+++ Algorithms/Tutorial/Trees/250. Count Univalue Subtrees/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var node1 = new TreeNode(5);
+            var node2 = new TreeNode(1);
+            var node3 = new TreeNode(5);
+            var node4 = new TreeNode(5);
+            var node5 = new TreeNode(1);
+            var node6 = new TreeNode(6);
+
+            node1.left = node2;
+            node1.right = node3;
+            node2.left = node4;
+            node2.right = node5;
+            node3.right = node6;
+
+            var solution = new Solution();
+            Console.WriteLine(solution.CountUnivalSubtrees(node1));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/Tutorial/Trees/250. Count Univalue Subtrees/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int CountUnivalSubtrees(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            int count = IsUniSubtree(root) 
+                + CountUnivalSubtrees(root.left) 
+                + CountUnivalSubtrees(root.right);
+
+            return count;
+        }
+
+        private int IsUniSubtree(TreeNode root)
+        {
+            int val = root.val;
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+
+                if (node.val != val)
+                    return 0;
+
+                if (node.right != null)
+                    stack.Push(node.right);
+                if (node.left != null)
+                    stack.Push(node.left);
+            }
+
+            return 1;
+        }
+    }
+}
