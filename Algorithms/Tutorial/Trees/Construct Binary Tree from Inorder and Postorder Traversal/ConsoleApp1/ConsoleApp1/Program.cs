846d6abc1c1257a756a7fec8138e638743767798
Andrii.Tkach <andreytkach85@gmail.com>
2020-12-08 23:49:35 +0100

    Construct Binary Tree from Inorder and Postorder Traversal

--- Algorithms/Tutorial/Trees/Construct Binary Tree from Inorder and Postorder Traversal/ConsoleApp1/ConsoleApp1/Program.cs
+++ Algorithms/Tutorial/Trees/Construct Binary Tree from Inorder and Postorder Traversal/ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,13 +13,26 @@ namespace ConsoleApp1
             
 
             var solution = new Solution();
-            solution.BuildTree(
+            var r1 = solution.BuildTree(
                 new int[] { 8, 4, 9, 2, 10, 5, 11, 1, 12, 6, 13, 3, 14, 7, 15 },
                 new int[] { 8, 9, 4, 10, 11, 5, 2, 12, 13, 6, 14, 15, 7, 3, 1 });
 
-            solution.BuildTree(
+            var r2 = solution.BuildTree(
                 new int[] { 9, 3, 15, 20, 7 }, 
                 new int[] { 9, 15, 7, 20, 3 });
+
+            var r3 = solution.BuildTree(
+                new int[] { 3, 2, 1 },
+                new int[] { 3, 2, 1 });
+
+
+            var r4 = solution.BuildTree(
+                new int[] { 2, 3, 1 },
+                new int[] { 3, 2, 1 });
+
+            var r5 = solution.BuildTree(
+                 new int[] { },
+                 new int[] { });
 
             Console.ReadLine();
         }
--- Algorithms/Tutorial/Trees/Construct Binary Tree from Inorder and Postorder Traversal/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Tutorial/Trees/Construct Binary Tree from Inorder and Postorder Traversal/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -6,12 +6,51 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    // taken from the solution
     public class Solution
     {
+        int post_idx;
+        int[] postorder;
+        int[] inorder;
+        Dictionary<int, int> idx_map = new Dictionary<int, int>();
+
+        public TreeNode helper(int in_left, int in_right)
+        {
+            // if there is no elements to construct subtrees
+            if (in_left > in_right)
+                return null;
+
+            // pick up post_idx element as a root
+            int root_val = postorder[post_idx];
+            TreeNode root = new TreeNode(root_val);
+
+            // root splits inorder list
+            // into left and right subtrees
+            int index = idx_map[root_val];
+
+            // recursion 
+            post_idx--;
+            // build right subtree
+            root.right = helper(index + 1, in_right);
+            // build left subtree
+            root.left = helper(in_left, index - 1);
+            return root;
+        }
+
         public TreeNode BuildTree(int[] inorder, int[] postorder)
         {
-            
+            idx_map.Clear();
 
+            this.postorder = postorder;
+            this.inorder = inorder;
+            // start from the last postorder element
+            post_idx = postorder.Length - 1;
+
+            // build a hashmap value -> its index
+            for (int i = 0; i < inorder.Length; i++)
+                idx_map.Add(inorder[i], i);
+
+            return helper(0, inorder.Length - 1);
         }
     }
 }
