e2887a12c07f28e0b337c8c90dfca4534129eec0
Andrii.Tkach <andreytkach85@gmail.com>
2020-12-21 23:29:26 +0100

    981. Time Based Key-Value

---
+++ Algorithms/Medium/HashTables/981. Time Based Key-Value Store/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1();
+            Test2();
+
+            Console.ReadLine();
+        }
+
+        private static void Test1()
+        {
+            var map = new TimeMap();
+            map.Set("love", "high", 10);
+            map.Set("love", "low", 20);
+
+            Console.WriteLine(map.Get("love", 5));
+            Console.WriteLine(map.Get("love", 10));
+            Console.WriteLine(map.Get("love", 15));
+            Console.WriteLine(map.Get("love", 20));
+            Console.WriteLine(map.Get("love", 25));
+        }
+
+        private static void Test2()
+        {
+            var map = new TimeMap();
+            map.Set("foo", "bar", 1);
+            Console.WriteLine(map.Get("foo", 1));
+            Console.WriteLine(map.Get("foo", 3));
+            map.Set("foo", "bar2", 4);
+            Console.WriteLine(map.Get("foo", 4));
+            Console.WriteLine(map.Get("foo", 5));
+        }
+    }
+}
---
+++ Algorithms/Medium/HashTables/981. Time Based Key-Value Store/ConsoleApp1/ConsoleApp1/TimeMap.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    public class TimeMap
+    {
+
+        Dictionary<string, SortedList<int, string>> _store;
+        public TimeMap()
+        {
+            _store = new Dictionary<string, SortedList<int, string>>();
+        }
+
+        public void Set(string key, string value, int timestamp)
+        {
+            if (_store.ContainsKey(key))
+            {
+                _store[key].Add(timestamp, value);
+            }
+            else
+            {
+                var list = new SortedList<int, string>();
+                list.Add(timestamp, value);
+                _store.Add(key, list);
+            }
+        }
+
+        public string Get(string key, int timestamp)
+        {
+            if (!_store.ContainsKey(key))
+                return string.Empty;
+
+            var values = _store[key];
+
+            int low = 0;
+            int high = values.Count;
+            int median = 0;
+            while (low < high)
+            {
+                median = (high - low) / 2 + low;
+
+                if (timestamp == values.Keys[median])
+                {
+                    return values.Values[median];
+                }
+
+                if (timestamp > values.Keys[median])
+                    low = median + 1;
+                if (timestamp < values.Keys[median])
+                    high = median - 1;
+            }
+
+            if (high < 0)
+                return string.Empty;
+
+            int medianTmsp = values.Keys[median] <= timestamp ? values.Keys[median] : 0;
+            int highTmsp = high < values.Count && values.Keys[high] <= timestamp ? values.Keys[high] : 0;
+            
+            if (medianTmsp == 0 && highTmsp == 0)
+                return string.Empty;
+
+            if (highTmsp > medianTmsp)
+                return values.Values[high];
+
+            if (medianTmsp > highTmsp)
+                return values.Values[median];
+
+            return string.Empty;
+        }
+    }
+
+    /**
+    * Your TimeMap object will be instantiated and called as such:
+    * TimeMap obj = new TimeMap();
+    * obj.Set(key,value,timestamp);
+    * string param_2 = obj.Get(key,timestamp);
+    */
+}
