39131c66f6ab2ba0c32a6bc78582bfe005a09074
Andrii.Tkach <andreytkach85@gmail.com>
2020-12-25 21:23:30 +0100

    535. Encode and Decode TinyURL

--- Algorithms/Medium/HashTables/380. Insert Delete GetRandom O(1)/ConsoleApp1/ConsoleApp1/RandomizedSet.cs
+++ Algorithms/Medium/HashTables/380. Insert Delete GetRandom O(1)/ConsoleApp1/ConsoleApp1/RandomizedSet.cs
@@ -8,6 +8,9 @@ namespace ConsoleApp1
 {
     public class RandomizedSet
     {
+        // 
+        private Dictionary<long, int> _map = new Dictionary<long, int>();
+        private List<long> _values = new List<long>(); 
 
         /** Initialize your data structure here. */
         public RandomizedSet()
@@ -18,20 +21,37 @@ namespace ConsoleApp1
         /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
         public bool Insert(int val)
         {
+            if (_map.ContainsKey(val))
+            {
+                if (_map[val] == int.MinValue)
+                {
+                    _map[val] = 
+                }
+
+                return false;
+            }
+
+            _map.Add(val, _values.Count);
+            _values.Add(val);
+
             return true;
         }
 
         /** Removes a value from the set. Returns true if the set contained the specified element. */
         public bool Remove(int val)
         {
+            //if (!_map.ContainsKey(val))
+            //    return false;
+
+            //var index = _map[val];
+            //_values.RemoveAt(index);
+
             return true;
         }
 
         /** Get a random element from the set. */
         public int GetRandom()
         {
-
-
             return 0;
         }
     }
---
+++ Algorithms/Medium/HashTables/535. Encode and Decode TinyURL/ConsoleApp1/ConsoleApp1/Codec.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Codec
+    {
+        private Dictionary<string, string> _map = new Dictionary<string, string>();
+
+        // Encodes a URL to a shortened URL
+        public string encode(string longUrl)
+        {
+            string key = "http://tinyurl.com/" + longUrl.GetHashCode().ToString();
+            _map.Add(key, longUrl);
+
+            return key;
+        }
+
+        // Decodes a shortened URL to its original URL.
+        public string decode(string shortUrl)
+        {
+            return _map[shortUrl];
+        }
+    }
+}
