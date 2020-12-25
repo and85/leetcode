9678664676efd4a72f455f78c45224b1973e9d2d
Andrii.Tkach <andreytkach85@gmail.com>
2020-12-25 21:48:21 +0100

    1244. Design A Leaderboard

---
+++ Algorithms/Medium/HashTables/1244. Design A Leaderboard/ConsoleApp1/ConsoleApp1/Leaderboard.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Leaderboard
+    {
+        private Dictionary<int, int> _scores = new Dictionary<int, int>();
+
+        public Leaderboard()
+        {
+
+        }
+
+        public void AddScore(int playerId, int score)
+        {
+            if (_scores.ContainsKey(playerId))
+            {
+                _scores[playerId] += score;
+            }
+            else
+            {
+                _scores.Add(playerId, score);
+            }
+        }
+
+        public int Top(int K)
+        {
+            var scores = _scores.Values.ToArray();
+           Array.Reverse(scores);
+            int sum = 0;
+            for (int i = 0; i < K; i++)
+            {
+                sum += scores[i];
+            }
+
+            return sum;
+        }
+
+        public void Reset(int playerId)
+        {
+            _scores[playerId] = 0;
+        }
+    }
+}
