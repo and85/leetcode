783dd9362d3f89c92b4cd9db047a055e807f90b2
Andrii.Tkach <andreytkach85@gmail.com>
2021-01-02 18:05:22 +0100

    134. Gas Station

---
+++ Algorithms/Medium/Greedy/134. Gas Station/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.CanCompleteCircuit(
+                new int[] { 1, 2, 3, 4, 5 },
+                new int[] { 3, 4, 5, 1, 2 }));
+
+            Console.WriteLine(solution.CanCompleteCircuit(
+                new int[] { 2, 3, 4 },
+                new int[] { 3, 4, 3 }));
+
+            var leetCodeSolution = new LeetCodeSolution();
+            Console.WriteLine(leetCodeSolution.canCompleteCircuit(
+                new int[] { 1, 2, 3, 4, 5 },
+                new int[] { 3, 4, 5, 1, 2 }));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/Medium/Greedy/134. Gas Station/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        private int _stationCount;
+        public int CanCompleteCircuit(int[] gas, int[] cost)
+        {
+            _stationCount = gas.Length;
+
+            for (int station = 0; station < _stationCount; station++)
+            {
+                if (CanTravelFromStation(station, gas, cost))
+                    return station;
+            }
+
+            return -1;
+        }
+
+        private bool CanTravelFromStation(int target, int[] gas, int[] cost)
+        {
+            int cur = target;
+            int tank = 0;
+
+            do
+            {
+                tank = FillTank(gas, cur, tank);
+
+                if (!HaveEnoughFuel(cost, cur, tank))
+                    return false;
+
+                MoveToNextStation(cost, ref cur, ref tank);
+            }
+            while (cur != target);
+
+            return true;
+        }
+
+        private int FillTank(int[] gas, int cur, int tank)
+        {
+            tank += gas[cur];
+            return tank;
+        }
+
+        private void MoveToNextStation(int[] cost, ref int cur, ref int tank)
+        {
+            tank -= cost[cur];
+            cur = GetNextStation(cur);
+        }
+
+        private bool HaveEnoughFuel(int[] cost, int cur, int tank)
+        {
+            return tank >= cost[cur];
+        }
+
+        private int GetNextStation(int station)
+        {
+            int nextStation = station + 1;
+            return nextStation < _stationCount ? nextStation : 0;
+        }
+    }
+}
