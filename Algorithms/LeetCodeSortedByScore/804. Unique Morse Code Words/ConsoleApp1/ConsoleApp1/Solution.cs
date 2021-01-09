b76525db756b5b5a741b917e2552c065548aa22d
Andrii.Tkach <andreytkach85@gmail.com>
2021-01-09 22:01:30 +0100

    804. Unique Morse Code Words

---
+++ Algorithms/LeetCodeSortedByScore/804. Unique Morse Code Words/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        private string[] _morseCodes = new string[26]
+        {
+            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", 
+            "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
+        };
+
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            var uniqueTransf = new HashSet<string>();
+            Array.ForEach(words, w => uniqueTransf.Add(GetTransformation(w)));
+
+            return uniqueTransf.Count;
+        }
+
+        private string GetTransformation(string word)
+        {
+            var sb = new StringBuilder();
+            foreach (char c in word)
+            {
+                sb.Append(_morseCodes[c - 97]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
