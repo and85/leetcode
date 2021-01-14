86e40af3fe9bf4ba58945f5fe5aae622e7748790
Andrii.Tkach <andreytkach85@gmail.com>
2021-01-14 22:57:29 +0100

    476. Number Complement

--- Algorithms/LeetCodeSortedByScore/476. Number Complement/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/LeetCodeSortedByScore/476. Number Complement/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Solution
+    public class Solution
     {
+        public int FindComplement(int num)
+        {
+            int reversed = ~num;
+
+            for (int i = 31; i >= 0; i--)
+            {
+                if (!GetBit(reversed, i))
+                    break;
+                reversed = ResetBit(reversed, i);
+            }
+
+            return reversed;
+        }
+
+        private bool GetBit(int num, int i)
+        {
+            return ((num & (1 << i)) != 0);
+        }
+
+        private int ResetBit(int num, int i)
+        {
+            int mask = ~(1 << i);
+            return num & mask;
+        }
     }
 }
