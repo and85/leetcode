2d95ae32d79d658d96bb2639181617708181b13b
and85 <andreytkach85@gmail.com>
2021-01-24 20:03:57 +0100

    77. Combinations

---
+++ Algorithms/Medium/Backtracking/77. Combinations/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            solution.Combine(4, 2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/Medium/Backtracking/77. Combinations/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        private int _n;
+        private int _k;
+
+        private IList<IList<int>> _result = new List<IList<int>>();
+
+        public IList<IList<int>> Combine(int n, int k)
+        {
+            _n = n;
+            _k = k;
+
+            Backtrack(1, new List<int>());
+            return _result;
+        }
+
+        private void Backtrack(int first, List<int> currentSolution)
+        {
+            if (currentSolution.Count == _k)
+                _result.Add(new List<int>(currentSolution));
+
+            for (int i = first; i <= _n; ++i)
+            {
+                // add i into the current combination
+                currentSolution.Add(i);
+
+                // use next integers to complete the combination
+                Backtrack(i + 1, currentSolution);
+
+                // backtrack
+                currentSolution.RemoveAt(currentSolution.Count - 1);
+
+            }
+        }
+    }
+}
