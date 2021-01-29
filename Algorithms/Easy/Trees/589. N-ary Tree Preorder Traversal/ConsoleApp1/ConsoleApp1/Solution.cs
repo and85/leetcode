96e6a940f60f2044a425fda3c52704a112ecd694
and85 <andreytkach85@gmail.com>
2021-01-29 21:45:13 +0100

    589. N-ary Tree Preorder Traversal

---
+++ Algorithms/Easy/Trees/589. N-ary Tree Preorder Traversal/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public IList<int> Preorder(Node root)
+        {
+            var result = new List<int>();
+
+            TraverseTree(root, result);
+
+            return result;
+
+        }
+
+        private void TraverseTree(Node root, List<int> result)
+        {
+            if (root == null)
+                return;
+
+            var stack = new Stack<Node>();
+            stack.Push(root);
+
+            while (stack.Count > 0)
+            {
+                
+                var node = stack.Pop();
+
+                result.Add(node.val);
+
+                foreach (var child in node.children.Reverse())
+                {
+                    stack.Push(child);
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms/Easy/Trees/589. N-ary Tree Preorder Traversal/ConsoleApp1/ConsoleApp1/SolutionRecursive.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class SolutionRecursive
+    {
+        public IList<int> Preorder(Node root)
+        {
+            var result = new List<int>();
+
+            TraverseTree(root, result);
+
+            return result;
+
+        }
+
+        private void TraverseTree(Node root, List<int> result)
+        {
+            if (root == null)
+                return;
+
+            result.Add(root.val);
+            foreach (var child in root.children)
+            {
+                TraverseTree(child, result);
+            }
+        }
+    }
+}
