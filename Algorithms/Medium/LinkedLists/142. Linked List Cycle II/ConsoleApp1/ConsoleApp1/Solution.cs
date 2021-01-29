1a6e3f827c6086e6fe7940685678e3bbae63b2cd
and85 <andreytkach85@gmail.com>
2021-01-29 23:04:41 +0100

    142. Linked List Cycle II. New approach

--- Algorithms/Medium/LinkedLists/142. Linked List Cycle II/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Medium/LinkedLists/142. Linked List Cycle II/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -8,19 +8,33 @@ namespace ConsoleApp1
     {
         public ListNode DetectCycle(ListNode head)
         {
-            //ListNode slow = head;
-            //ListNode fast = head;
 
-            //while (slow != null && fast != null)
-            //{
 
-            //    slow = slow.next;
-            //    fast = fast.next?.next;
+            //return HashSetSolution(head);
 
-            //    if (slow == fast)
-            //        return slow;
-            //}
+            ListNode curr = head;
 
+            while (curr != null)
+            {
+                if (curr.val > 100000)
+                {
+                    curr.val -= 200001;
+                    return curr;
+                }
+
+                // mark as visited
+                curr.val += 200001;
+
+                curr = curr.next;
+            }
+
+            return null;
+
+
+        }
+
+        private static ListNode HashSetSolution(ListNode head)
+        {
             ListNode slow = head;
             HashSet<ListNode> hashSet = new HashSet<ListNode>();
 
