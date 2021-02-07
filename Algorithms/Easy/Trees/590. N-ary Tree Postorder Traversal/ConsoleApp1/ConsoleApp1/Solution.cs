d28954fa3b188470ddaa68c0795e1da16d2f68dc
and85 <andreytkach85@gmail.com>
2021-02-07 18:41:52 +0100

    590. N-ary Tree Postorder Traversal 429. N-ary Tree Level Order Traversal

---
+++ Algorithms/Easy/Trees/590. N-ary Tree Postorder Traversal/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public IList<int> Postorder(Node root)
+        {
+            return Traverse(root, new List<int>());
+        }
+
+        public IList<int> Traverse(Node root, List<int> res)
+        {
+            if (root == null)
+                return res;
+
+            foreach (var child in root.children)
+                Traverse(child, res);
+
+            res.Add(root.val);
+
+            return res;
+        }
+    }
+}
---
+++ Algorithms/Medium/Tree/429. N-ary Tree Level Order Traversal/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public class Level
+        {
+            public Level(Node node, int height)
+            {
+                Node = node;
+                Height = height;
+            }
+
+            public Node Node { get; set; }
+            public int Height { get; set; }
+        }
+
+        public IList<IList<int>> LevelOrder(Node root)
+        {
+            var result = new List<IList<int>>();
+            if (root == null)
+                return result;
+
+            var queue = new Queue<Level>();
+            int height = 1;
+            queue.Enqueue(new Level(root, height));
+
+            while (queue.Count > 0)
+            {
+                var level = new List<int>();
+
+                while (queue.Count > 0 && queue.Peek().Height == height)
+                {
+                    var curNode = queue.Dequeue();
+
+                    foreach (var child in curNode.Node.children)
+                    {
+                        queue.Enqueue(new Level(child, height + 1));
+                    }
+
+                    level.Add(curNode.Node.val);
+                }
+
+                height++;
+
+                result.Add(level);
+            }
+
+            return result;
+        }
+    }
+}
