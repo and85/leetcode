aa7698c722566a74dd5706039090f7fbd95175f4
and85 <andreytkach85@gmail.com>
2021-02-17 22:52:47 +0100

    207. Course Schedule

--- Algorithms/Medium/Graph/207. Course Schedule/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Medium/Graph/207. Course Schedule/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System    ;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,36 +7,85 @@ namespace ConsoleApp1
     // https://leetcode.com/problems/course-schedule/
     public class Solution
     {
-        public bool CanFinish(int numCourses, int[][] prerequisites)
+        public class Graph
         {
-            var uniqueCoures = new HashSet<int>();
-            var courcesDictionary = new Dictionary<int, int>();
-            foreach (int[] p in prerequisites)
-            {
-                int course = p[0];
-                int prerequisite = p[1];
-                if (!uniqueCoures.Contains(course))
-                    uniqueCoures.Add(course);
-                if (!uniqueCoures.Contains(prerequisite))
-                    uniqueCoures.Add(prerequisite);
+            private readonly int V;
+            private readonly List<List<int>> adj;
 
-                // check number of taken cources
-                if (uniqueCoures.Count > numCourses)
+            public Graph(int V)
+            {
+                this.V = V;
+                adj = new List<List<int>>(V);
+
+                for (int i = 0; i < V; i++)
+                    adj.Add(new List<int>());
+            }
+
+            // This function is a variation of DFSUtil() in  
+            // https://www.geeksforgeeks.org/archives/18212  
+            private bool isCyclicUtil(int i, bool[] visited,
+                                            bool[] recStack)
+            {
+
+                // Mark the current node as visited and  
+                // part of recursion stack  
+                if (recStack[i])
+                    return true;
+
+                // in fact it's a small optimization which prevents us from processing a vertex when it was already visited and checked
+                if (visited[i])
                     return false;
 
-                // check self references
-                if (!courcesDictionary.ContainsKey(course))
-                    courcesDictionary.Add(course, prerequisite);
+                visited[i] = true;
 
-                if (courcesDictionary.ContainsKey(prerequisite))
-                {
-                    if (courcesDictionary[prerequisite] == course)
-                        return false;
-                }
+                recStack[i] = true;
+                List<int> children = adj[i];
+
+                foreach (int c in children)
+                    if (isCyclicUtil(c, visited, recStack))
+                        return true;
+
+                recStack[i] = false;
+
+                return false;
             }
-            
 
-            return true;
+            public void AddEdge(int sou, int dest)
+            {
+                adj[sou].Add(dest);
+            }
+
+            // Returns true if the graph contains a  
+            // cycle, else false.  
+            // This function is a variation of DFS() in  
+            // https://www.geeksforgeeks.org/archives/18212  
+            public bool isCyclic()
+            {
+
+                // Mark all the vertices as not visited and  
+                // not part of recursion stack  
+                bool[] visited = new bool[V];
+                bool[] recStack = new bool[V];
+
+                // Call the recursive helper function to  
+                // detect cycle in different DFS trees  
+                for (int i = 0; i < V; i++)
+                    if (isCyclicUtil(i, visited, recStack))
+                        return true;
+
+                return false;
+            }
+        }
+
+        public bool CanFinish(int numCourses, int[][] prerequisites)
+        {
+            Graph graph = new Graph(numCourses);
+
+            for (int i = 0; i < prerequisites.Length; i++)
+                graph.AddEdge(prerequisites[i][0], prerequisites[i][1]);
+
+
+            return !graph.isCyclic();
         }
     }
 }
