44997a270ff4a3fd0894684c96250769cfa9ac16
and85 <andreytkach85@gmail.com>
2021-02-23 17:49:24 +0100

    230. Kth Smallest Element in a BST

---
+++ Algorithms/Medium/Tree/230. Kth Smallest Element in a BST/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        private int _k;
+        private int _counter;
+        private int _res;
+        public int KthSmallest(TreeNode root, int k)
+        {
+            _k = k;
+            InodrderTraversal(root);
+
+            return _res;
+        }
+
+        private void InodrderTraversal(TreeNode root)
+        {
+            if (root == null)
+                return;
+
+            InodrderTraversal(root.left);
+
+            if (++_counter == _k)
+            {
+                _res = root.val;
+                return;
+            }
+
+            InodrderTraversal(root.right);
+        }
+    }
+}
