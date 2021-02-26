5e6ef2a16aac2c13198dc351805f4e4497c5bbce
and85 <andreytkach85@gmail.com>
2021-02-26 23:46:52 +0100

    1351. Count Negative Numbers in a Sorted Matrix

---
+++ Algorithms/Easy/Arrays/1351. Count Negative Numbers in a Sorted Matrix/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int CountNegatives(int[][] grid)
+        {
+            int counter = 0, index;
+
+            for (int r = 0; r < grid.Length; r++)
+            {
+                index = BinarySearch(grid[r]);
+                counter += grid[0].Length - index;
+            }
+
+            return counter;
+        }
+        
+        private int BinarySearch(int[] row)
+        {
+            int low = 0;
+            int high = row.Length - 1;
+
+            while (low <= high)
+            {
+                int median = low + (high - low) / 2;	
+
+                if (row[median] >= 0)
+                {
+                    low = median + 1;
+                }
+                else
+                {
+                    high = median - 1;
+                }
+            }
+
+            return low;
+
+        }
+    }
+}
---
+++ Algorithms/Easy/Arrays/1351. Count Negative Numbers in a Sorted Matrix/ConsoleApp1/NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using ConsoleApp1;
+
+namespace NUnitTestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // arrange
+            var grid = new int[][]
+            {
+                new int[] { 4, 3, 2, -1 },
+                new int[] { 3,2,1,-1 },
+                new int[] { 1,1,-1,-2 },
+                new int[] { -1,-1,-2,-3 } 
+            };
+            var solution = new Solution();
+
+            // act
+            int actual = solution.CountNegatives(grid);
+
+            // assert
+            Assert.AreEqual(8, actual);
+        }
+    }
+}
