9cb778cf5ea00ff051e4c450caf17d350cac0a28
and85 <andreytkach85@gmail.com>
2021-03-04 18:06:02 +0100

    235. Lowest Common Ancestor of a Binary Search Tree

--- Algorithms/Easy/Trees/235. Lowest Common Ancestor of a Binary Search Tree/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Easy/Trees/235. Lowest Common Ancestor of a Binary Search Tree/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -8,7 +8,28 @@ namespace ConsoleApp1
     {
         public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
         {
+            /*
+                1. if root equal to p or q -> found. root is the lowest common  ancestor
+                2. if left is less, right is greater -> found. root is the lowest common  ancestor
+                3. if p and q is less than root, check left sub tree
+                4. if p and q is greater than root, check right subtree
+            */
+            if (root.val == p.val || root.val == q.val)
+                return root;
 
+            int l = Math.Min(p.val, q.val);
+            int r = Math.Max(p.val, q.val);
+
+            if (root.val > l && root.val < r)
+                return root;
+
+            if (l < root.val && r < root.val)
+                return LowestCommonAncestor(root.left, p, q);
+
+            if (l > root.val && r > root.val)
+                return LowestCommonAncestor(root.right, p, q);
+
+            return null;
         }
     }
 }
