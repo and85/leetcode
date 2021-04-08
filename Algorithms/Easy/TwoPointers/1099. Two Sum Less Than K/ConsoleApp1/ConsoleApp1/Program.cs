4c1fa388585f2c5a65e29068f205a6837a32d402
and85 <andreytkach85@gmail.com>
2021-04-08 23:31:42 +0200

    1099. Two Sum Less Than K

---
+++ Algorithms/Easy/TwoPointers/1099. Two Sum Less Than K/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            s.TwoSumLessThanK(new int[] { 34, 23, 1, 24, 75, 33, 54, 8 }, 60);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/Easy/TwoPointers/1099. Two Sum Less Than K/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int TwoSumLessThanK(int[] nums, int k)
+        {
+            Array.Sort(nums);
+
+            int left = 0, right = nums.Length - 1, curSum, maxSum = 0;
+
+            while (left < right)
+            {
+                curSum = nums[left] + nums[right];                
+
+                if (curSum < k)
+                {
+                    maxSum = Math.Max(maxSum, curSum);
+                    left++;
+                }
+
+                if (curSum >= k)
+                {
+                    right--;
+                }
+            }
+
+            return maxSum > 0 ? maxSum : -1;
+        }
+    }
+}
