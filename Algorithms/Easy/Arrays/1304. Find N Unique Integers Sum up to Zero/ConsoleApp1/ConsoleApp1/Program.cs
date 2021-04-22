cd7846fd6bd0518af82ec83a3db599e8969bb750
and85 <andreytkach85@gmail.com>
2021-04-22 23:17:30 +0200

    543. Diameter of Binary Tree 1304. Find N Unique Integers Sum up to Zero

---
+++ Algorithms/Easy/Arrays/1304. Find N Unique Integers Sum up to Zero/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            s.SumZero(6);
+            s.SumZero(5);
+            s.SumZero(3);
+            s.SumZero(1);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/Easy/Arrays/1304. Find N Unique Integers Sum up to Zero/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int[] SumZero(int n)
+        {
+            
+            bool even = n % 2 == 0;
+
+            int[] res = new int[n];
+            
+                for (int i = 0; i < n - (even ? 0 : 1); i += 2)
+                {
+                    res[i] = i + 1;
+                    res[i + 1] = -res[i];
+                }
+            
+
+            return res;
+        }
+    }
+}
