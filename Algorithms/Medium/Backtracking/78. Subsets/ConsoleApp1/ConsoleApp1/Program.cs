ffc944eef87848d9a03a831cf6dd1861b7d2053d
and85 <andreytkach85@gmail.com>
2021-07-27 17:56:33 +0200

    78. Subsets

---
+++ Algorithms/Medium/Backtracking/78. Subsets/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            solution.Subsets(new int[] { 1, 2, 3});
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/Medium/Backtracking/78. Subsets/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        private IList<IList<int>> _result = new List<IList<int>>();
+
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            var result = new List<IList<int>>();            
+
+            var solution = new List<int>();
+
+            Backtrack(nums, 0, solution);
+
+            return _result;
+        }
+
+        private void Backtrack(int[] nums, int first, List<int> currentSolution)
+        {            
+            _result.Add(new List<int>(currentSolution));
+
+            for (int i = first; i < nums.Length; ++i)
+            {
+                // add nums[i] into the current combination
+                currentSolution.Add(nums[i]);
+
+                // use next integers to complete the combination
+                Backtrack(nums, i + 1, currentSolution);
+
+                // backtrack
+                currentSolution.RemoveAt(currentSolution.Count - 1);
+            }
+        }
+    }
+}
