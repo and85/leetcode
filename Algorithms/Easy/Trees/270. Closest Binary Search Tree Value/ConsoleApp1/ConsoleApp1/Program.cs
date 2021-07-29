9f654d0b54d3943924c9c9a7c33d7ca1a17ebf03
and85 <andreytkach85@gmail.com>
2021-07-29 23:10:31 +0200

    270. Closest Binary Search Tree Value

---
+++ Algorithms/Easy/Trees/270. Closest Binary Search Tree Value/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var t4 = new TreeNode(4);
+            var t2 = new TreeNode(2);
+            var t5 = new TreeNode(5);
+            var t1 = new TreeNode(1);
+            var t3 = new TreeNode(3);
+
+            t4.left = t2;
+            t4.right = t5;
+            t2.left = t1;
+            t2.right = t3;
+
+            var solution = new Solution();
+            solution.ClosestValue(t4, 3.714286);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/Easy/Trees/270. Closest Binary Search Tree Value/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public int ClosestValue(TreeNode root, double target)
+        {
+            int result = -1;
+            double minDiff = double.PositiveInfinity;
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var curNode = queue.Dequeue();
+
+                if (target == curNode.val) return curNode.val;
+
+                double curDiff = Math.Abs(target - curNode.val);
+                if (curDiff < minDiff)
+                {
+                    minDiff = curDiff;
+                    result = curNode.val;
+                }
+
+                if (target < curNode.val && curNode.left != null)
+                    queue.Enqueue(curNode.left);
+
+                if (target > curNode.val && curNode.right != null)
+                    queue.Enqueue(curNode.right);
+            }
+
+            return result;
+        }
+    }
+}
