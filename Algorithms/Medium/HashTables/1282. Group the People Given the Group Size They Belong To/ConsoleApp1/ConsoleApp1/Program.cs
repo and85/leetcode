a53b9cc9fd274bb5998eb8c6e28f4e8e18b87822
and85 <andreytkach85@gmail.com>
2021-08-17 17:40:28 +0200

    1282. Group the People Given the Group Size They Belong To

---
+++ Algorithms/Medium/HashTables/1282. Group the People Given the Group Size They Belong To/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            solution.GroupThePeople(new int[] { 3, 3, 3, 3, 3, 1, 3 });
+
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/Medium/HashTables/1282. Group the People Given the Group Size They Belong To/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public IList<IList<int>> GroupThePeople(int[] groupSizes)
+        {
+            var groups = new Dictionary<int, IList<IList<int>>>();            
+
+            for (int i = 0; i < groupSizes.Length; i++)
+            {
+                int size = groupSizes[i];
+                if (!groups.ContainsKey(size))
+                {
+                    groups.Add(size, new List<IList<int>>());
+                    groups[size].Add(new List<int>());                    
+                }
+                else
+                {
+                    if (groups[size].Last().Count == size)
+                        groups[size].Add(new List<int>());                    
+                }
+
+                groups[size].Last().Add(i);
+
+            }
+
+
+            return groups.Values.SelectMany(g => g).ToList();
+        }
+    }
+}
