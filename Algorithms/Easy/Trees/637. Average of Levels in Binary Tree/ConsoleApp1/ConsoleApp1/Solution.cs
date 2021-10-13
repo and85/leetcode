9755d41a7f1b82c6cd7ef850fc24a91f26ef4118
and85 <andreytkach85@gmail.com>
2021-10-13 22:54:11 +0200

    637. Average of Levels in Binary Tree

---
+++ Algorithms/Easy/Trees/637. Average of Levels in Binary Tree/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public IList<double> AverageOfLevels(TreeNode root)
+        {
+            var result = new List<double>();
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                int levelSize = queue.Count();
+
+                double sum = 0;
+                for (int i = 0; i < levelSize; i++)
+                {
+                    var node = queue.Dequeue();
+                    sum += node.val;
+
+                    if (node.left != null)
+                        queue.Enqueue(node.left);
+                    if (node.right != null)
+                        queue.Enqueue(node.right);
+                }
+
+                result.Add(sum / levelSize);
+            }
+
+            return result;
+        }
+    }
+}
