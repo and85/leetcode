b3050454c08457337d83a2f28773068f2d7eeeb4
and85 <andreytkach85@gmail.com>
2022-06-20 23:50:26 +0200

    617. Merge Two Binary Trees

--- Algorithms/Easy/Trees/617. Merge Two Binary Trees/ConsoleApp1/ConsoleApp1/Program.cs
+++ Algorithms/Easy/Trees/617. Merge Two Binary Trees/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,31 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var left1 = new TreeNode(1);
+            var left3 = new TreeNode(3);
+            var left2 = new TreeNode(2);
+            var left5 = new TreeNode(5);
+
+            left1.left = left3;
+            left1.right = left2;
+            left3.left = left5;
+
+            var right2 = new TreeNode(2);
+            var right1 = new TreeNode(1);
+            var right3 = new TreeNode(3);
+            var right4 = new TreeNode(4);
+            var right7 = new TreeNode(7);
+
+            right2.left = right1;
+            right2.right = right3;
+            right1.right = right4;
+            right3.right = right7;
+
+            var s = new Solution();
+            var res = s.MergeTrees(left1, right2);
+
+            s.MergeTrees(null, new TreeNode(1));
+
         }
     }
 }
--- Algorithms/Easy/Trees/617. Merge Two Binary Trees/ConsoleApp1/ConsoleApp1/Solution.cs
+++ Algorithms/Easy/Trees/617. Merge Two Binary Trees/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -6,6 +6,64 @@ namespace ConsoleApp1
 {
     public class Solution
     {
+        public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        {
+            if (t1 == null)
+                return t2;
+
+            var queue = new Queue<(TreeNode, TreeNode)>();
+            queue.Enqueue((t1, t2));
+
+            while (queue.Count != 0)
+            {
+                var pair = queue.Dequeue();
+
+                if (pair.Item1 == null || pair.Item2 == null)
+                {
+                    continue;
+                }
+
+                pair.Item1.val += pair.Item2?.val ?? 0;
+                
+
+                if (pair.Item1?.left == null)
+                {
+                    //if (pair.Item1 == null) pair.Item1 = new TreeNode();
+                    pair.Item1.left = pair.Item2?.left;
+                }
+                else
+                {
+                    queue.Enqueue((pair.Item1?.left, pair.Item2?.left));
+                }
+
+                if (pair.Item1?.right == null)
+                {
+                    //if (pair.Item1 == null) pair.Item1 = new TreeNode();
+                    pair.Item1.right = pair.Item2?.right;
+                }
+                else
+                {
+                    queue.Enqueue((pair.Item1?.right, pair.Item2?.right));
+                }
+            }
+
+
+            return t1;
+        }
+
+        public TreeNode MergeTrees_attempt3(TreeNode t1, TreeNode t2)
+        {
+            if (t1 == null && t2 == null) return null;
+            
+            int mergedValue = (t1?.val ?? 0) + (t2?.val ?? 0);
+
+            TreeNode node = new TreeNode(mergedValue);
+            node.left = MergeTrees(t1?.left, t2?.left);
+            node.right = MergeTrees(t1?.right, t2?.right);
+
+            return node;
+        }
+
         public TreeNode MergeTrees_attempt1(TreeNode t1, TreeNode t2)
         {
             if (t1 == null && t2 == null)
@@ -20,7 +78,7 @@ namespace ConsoleApp1
             return newTree;
         }
 
-        public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        public TreeNode MergeTrees_attempt2(TreeNode t1, TreeNode t2)
         {
             if (t1 == null && t2 != null) return t2;
 
